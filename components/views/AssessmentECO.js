a3bf0ff129c397fb3bfe86da616949038993112b
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2021-06-25 15:58:54 +0200

    Add checkbox for ECO assessment

--- components/views/AssessmentECO.js
+++ components/views/AssessmentECO.js
@@ -6,32 +6,58 @@ export class AssessmentECO extends React.Component {
 
   constructor(props) {
     super(props);
+    let indicator = props.session.getValueAddedFootprint("eco"); 
     this.state = {
-      indicator: props.session.getValueAddedFootprint("eco"),
+      indicator: indicator,
+      domesticProductionInput: indicator.getDomesticProduction()!=null ? indicator.getDomesticProduction() : "",
     }
   }
 
   render() {
-    const indicator = this.state.indicator;
+    const {indicator,domesticProductionInput} = this.state;
+    const isAllActivitiesInFrance = indicator.getIsAllActivitiesInFrance()!=null ? indicator.getIsAllActivitiesInFrance() : false;
     return (
-      <table>
-        <thead>
-          <tr><td>Libelle</td><td>Valeur</td></tr>
-        </thead>
-        <tbody>
-          <tr className="with-bottom-line"><td>Valeur ajoutée nette produite en France (en €)</td>
-              <td className="column_value"><NumberInput value={indicator.getDomesticProduction()}
-                                onBlur={this.updateDomesticProduction.bind(this)}/></td></tr>
-          <tr><td>Valeur ajoutée nette (en €)</td>
-              <td className="column_value"><input value={printValue(indicator.getNetValueAdded(),0)} disabled={true}></input></td></tr>
-          <tr><td>Contribution directe (en %)</td>
-              <td className="column_value"><input value={printValue(indicator.getValue(),1)} disabled={true}/></td></tr>
-        </tbody>
-      </table>
+      <div>
+        <table>
+          <thead>
+            <tr><td>Libelle</td><td>Valeur</td></tr>
+          </thead>
+          <tbody>
+            <tr><td>Activités localisées en France</td>
+                <td><input type="checkbox"
+                              checked={isAllActivitiesInFrance} 
+                              onChange={this.onIsAllActivitiesInFranceBlur}/></td></tr>
+            <tr className="with-bottom-line"><td>Valeur ajoutée nette produite en France (en €)</td>
+                <td className="column_value">
+                  <input value={isAllActivitiesInFrance ? printValue(indicator.getNetValueAdded(),0) : domesticProductionInput}
+                        onChange={this.onDomesticProductionChange}
+                        onBlur={this.onDomesticProductionBlur}
+                        onKeyPress={this.onEnterPress}/></td></tr>
+            <tr><td>Valeur ajoutée nette (en €)</td>
+                <td className="column_value"><input value={printValue(indicator.getNetValueAdded(),0)} disabled={true}></input></td></tr>
+            <tr><td>Contribution directe (en %)</td>
+                <td className="column_value"><input value={printValue(indicator.getValue(),1)} disabled={true}/></td></tr>
+          </tbody>
+        </table>
+      </div>
     ) 
   }
 
-  updateDomesticProduction = (event) => {
+  onEnterPress = (event) => {
+    if (event.which==13) {event.target.blur();}
+  }
+
+  onIsAllActivitiesInFranceBlur = (event) => {
+    this.state.indicator.setIsAllActivitiesInFrance(event.target.checked);
+    this.state.domesticProductionInput = this.state.indicator.getDomesticProduction();
+    this.props.onUpdate(this.state.indicator);
+  }
+
+  onDomesticProductionChange = (event) => {
+    this.state.indicator.setIsAllActivitiesInFrance(false);
+    this.setState({domesticProductionInput: event.target.value});
+  }
+  onDomesticProductionBlur = (event) => {
     let domesticProduction = parseFloat(event.target.value);
     this.state.indicator.setDomesticProduction(!isNaN(domesticProduction) ? domesticProduction : null);
     this.props.onUpdate(this.state.indicator);
--- src/indicator/IndicatorECO.js
+++ src/indicator/IndicatorECO.js
@@ -8,27 +8,49 @@ export class IndicatorECO extends IndicatorNetValueAdded {
     super("eco");
     // Specific data for ECO
     this.domesticProduction = null;
+    this.isAllActivitiesInFrance = null;
   }
 
   updateFromBackUp(backUp) {
     super.updateFromBackUp(backUp);
     this.domesticProduction = backUp.domesticProduction;
+    // 24-06-2021
+    this.isAllActivitiesInFrance = backUp.isAllActivitiesInFrance!=undefined ? backUp.isAllActivitiesInFrance : (this.domesticProduction==this.netValueAdded);
   }
 
+  /* ----- SETTERS ----- */
+
   setDomesticProduction(domesticProduction) {
-    if (this.value!=null) {
-      this.value = this.value*(domesticProduction/this.domesticProduction);
-    }
     this.domesticProduction = domesticProduction;
     this.uncertainty = 0;
+    this.isAllActivitiesInFrance = domesticProduction==this.netValueAdded;
   }
+
+  setIsAllActivitiesInFrance(isAllActivitiesInFrance) {
+    this.isAllActivitiesInFrance = isAllActivitiesInFrance;
+    if (isAllActivitiesInFrance) {
+      this.domesticProduction = this.netValueAdded;
+    } else {
+      this.domesticProduction = 0.0;
+    }
+  }
+
+  /* ----- GETTERS ----- */
 
   getDomesticProduction() {
     return this.domesticProduction;
   }
 
+  getIsAllActivitiesInFrance() {
+    return this.isAllActivitiesInFrance;
+  }
+
+  /* ----- VALUE & UNCERTAINTY ----- */
+
   getValue() {
-    if (this.netValueAdded!=null & this.domesticProduction!=null) {
+    if (this.netValueAdded!=null & this.isAllActivitiesInFrance) {
+      return 100.0;
+    } else if (this.netValueAdded!=null & this.domesticProduction!=null) {
       if (this.netValueAdded < this.domesticProduction) {
           this.domesticProduction = this.netValueAdded;
       }
