4154b7caa5c464d0166f27783e7873d5b7a6ea1d
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2021-06-28 13:20:50 +0200

    Add notes in SOC assessment

--- components/views/AssessmentSOC.js
+++ components/views/AssessmentSOC.js
@@ -12,24 +12,39 @@ export class AssessmentSOC extends React.Component {
   render() {
     const indicator = this.state.indicator;
     return (
-      <table>
-        <thead>
-          <tr><td>Libelle</td><td>Valeur</td></tr>
-        </thead>
-        <tbody>
-          <tr className="with-bottom-line">
-            <td>Raison d'être - Intérêt social (oui/non)</td>
-            <td><input type="checkbox"
-                        checked={indicator.getHasSocialPurpose()!=null ? indicator.getHasSocialPurpose() : false} 
-                        onChange={this.onHasSocialPurposeChange}/></td></tr>
-          <tr>
-            <td>Valeur ajoutée nette (en €)</td>
-            <td className="column_value"><input value={printValue(indicator.getNetValueAdded(),0)} disabled={true}/></td></tr>
-          <tr>
-            <td>Contribution directe (en %)</td>
-            <td className="column_value"><input value={printValue(indicator.getValue(),1)} disabled={true}/></td></tr>
-        </tbody>
-      </table>
+      <div>
+        <table>
+          <thead>
+            <tr><td>Libelle</td><td>Valeur</td></tr>
+          </thead>
+          <tbody>
+            <tr className="with-bottom-line">
+              <td>Raison d'être défini dans les statuts ? Entreprise de l'ESS ou d'utilité sociale ?</td>
+              <td><input type="checkbox"
+                          checked={indicator.getHasSocialPurpose()!=null ? indicator.getHasSocialPurpose() : false} 
+                          onChange={this.onHasSocialPurposeChange}/></td></tr>
+            <tr>
+              <td>Valeur ajoutée nette (en €)</td>
+              <td className="column_value"><input value={printValue(indicator.getNetValueAdded(),0)} disabled={true}/></td></tr>
+            <tr>
+              <td>Contribution directe (en %)</td>
+              <td className="column_value"><input value={printValue(indicator.getValue(),1)} disabled={true}/></td></tr>
+          </tbody>
+        </table>
+        <div>
+          <h3>Notes</h3>
+          <p>Grandeur mesurée : Valeur nette créée par des acteurs d’intérêt social (en euros)</p>
+          <p>L’impact direct à l’échelle d’une unité légale est nul ou équivalent à la valeur ajoutée nette selon l'existence ou non d’un intérêt social.</p>
+          <p>Critères :<br/>
+            - Structure de l’ESS (Economie Sociale et Solidaire)<br/>
+            - Agrément ESUS (Entreprise Solidaire d'Utilité Sociale)<br/>
+            - Entreprise à mission / Raison d’être inscrite dans les statuts de l’entreprise</p>
+          <p>
+            <a href="https://www.economie.gouv.fr/cedef/economie-sociale-et-solidaire" target="_blank">Qu'est-ce que l'économie sociale et solidaire ?</a><br/>
+            <a href="https://www.economie.gouv.fr/entreprises/agrement-entreprise-solidaire-utilite-sociale-ess" target="_blank">Qu’est-ce que l’agrément « Entreprise solidaire d’utilité sociale » ?</a><br/>
+            <a href="https://www.economie.gouv.fr/cedef/societe-mission" target="_blank">Que sont les sociétés à mission ?</a></p>
+        </div>
+      </div>
     ) 
   }
 
