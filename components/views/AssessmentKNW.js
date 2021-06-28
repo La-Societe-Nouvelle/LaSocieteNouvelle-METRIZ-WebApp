c4cc07d13d1338b6faed3d3a0311f6552a83bc23
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2021-06-28 13:20:06 +0200

    Add notes in KNW assessment

--- components/views/AssessmentKNW.js
+++ components/views/AssessmentKNW.js
@@ -14,24 +14,36 @@ export class AssessmentKNW extends React.Component {
   render() {
     const indicator = this.state.indicator;
     return (
-      <table>
-        <thead>
-          <tr><td>Libelle</td><td>Valeur</td></tr>
-        </thead>
-        <tbody>
-          <tr className="with-bottom-line">
-            <td>Rémunérations brutes de recherche et formation (en €)</td>
-            <td className="column_value"><NumberInput 
-              value={indicator.getSpendings()} 
-              onBlur={this.updateTotalContribution.bind(this)}/></td>
-          </tr><tr>
-            <td>Valeur ajoutée nette (en €)</td>
-            <td className="column_value"><input value={printValue(indicator.getNetValueAdded(),0)} disabled={true}/></td></tr>
-          <tr>
-            <td>Contribution directe (en %)</td>
-            <td className="column_value"><input value={printValue(indicator.getValue(),1)} disabled={true}/></td></tr>
-        </tbody>
-      </table>
+      <div>
+        <table>
+          <thead>
+            <tr><td>Libelle</td><td>Valeur</td></tr>
+          </thead>
+          <tbody>
+            <tr className="with-bottom-line">
+              <td>Rémunérations brutes de recherche et formation (en €)</td>
+              <td className="column_value"><NumberInput 
+                value={indicator.getSpendings()} 
+                onBlur={this.updateTotalContribution.bind(this)}/></td>
+            </tr><tr>
+              <td>Valeur ajoutée nette (en €)</td>
+              <td className="column_value"><input value={printValue(indicator.getNetValueAdded(),0)} disabled={true}/></td></tr>
+            <tr>
+              <td>Contribution directe (en %)</td>
+              <td className="column_value"><input value={printValue(indicator.getValue(),1)} disabled={true}/></td></tr>
+          </tbody>
+        </table>
+        <div>
+          <h3>Notes</h3>
+          <p>Grandeur mesurée : Montant total des charges (hors charges externes) liées à la formation, l’éducation ou la recherche (en euros)</p>
+          <p>L’impact direct à l’échelle d’une unité légale est nul ou équivalent à la valeur ajoutée nette selon l'existence ou non d’un intérêt social.</p>
+          <p>Les postes de charges sont les suivants :<br/>
+            - Rémunérations de contrat de formation : stage, alternance, contrat d’apprentissage<br/>
+            - Rémunérations liées à l’encadrement de formation<br/>
+            - Rémunérations liées à des travaux de recherche<br/>
+            - Rémunérations liées à des partenariats avec des établissements de formation</p>
+        </div>
+      </div>
     ) 
   }
 
