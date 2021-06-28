d8defcdf64fb62bd69c2c3b8e2bfa44585ee7255
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2021-06-28 13:19:25 +0200

    Add notes in HAZ assessment

--- components/views/AssessmentHAZ.js
+++ components/views/AssessmentHAZ.js
@@ -15,32 +15,43 @@ export class AssessmentHAZ extends React.Component {
   render() {
     const {indicator,versionItems} = this.state;
     return (
-      <table>
-        <thead>
-          <tr><td>Libelle</td><td>Valeur</td><td>Incertitude</td></tr>
-        </thead>
-        <tbody>
-          <tr className="with-bottom-line">
-            <td>Utilisation de produits dangereux - santé/environnement (en kg)</td>
-            <td className="column_value"><NumberInput 
-              key={versionItems}
-              value={indicator.getProductsUse()}
-              onBlur={this.updateProductsUse.bind(this)}/></td>
-            <td className="column_value"><NumberInput
-              key={versionItems}
-              value={indicator.getProductsUseUncertainty()} 
-              onBlur={this.updateUncertainty.bind(this)}/></td>
-          </tr>
-          <tr>
-            <td>Valeur ajoutée nette (en €)</td>
-            <td className="column_value"><input value={printValue(indicator.getNetValueAdded(),0)} disabled={true}/></td></tr>
-          <tr>
-            <td>Intensité (en g/€)</td>
-            <td className="column_value"><input value={printValue(indicator.getValue(),1)} disabled={true}/></td>
-            <td className="column_value"><input value={printValue(indicator.getUncertainty(),0)} disabled={true}/></td>
-          </tr>
-        </tbody>
-      </table>
+      <div>
+        <table>
+          <thead>
+            <tr><td>Libelle</td><td>Valeur</td><td>Incertitude</td></tr>
+          </thead>
+          <tbody>
+            <tr className="with-bottom-line">
+              <td>Utilisation de produits dangereux - santé/environnement (en kg)</td>
+              <td className="column_value"><NumberInput 
+                key={versionItems}
+                value={indicator.getProductsUse()}
+                onBlur={this.updateProductsUse.bind(this)}/></td>
+              <td className="column_value"><NumberInput
+                key={versionItems}
+                value={indicator.getProductsUseUncertainty()} 
+                onBlur={this.updateUncertainty.bind(this)}/></td>
+            </tr>
+            <tr>
+              <td>Valeur ajoutée nette (en €)</td>
+              <td className="column_value"><input value={printValue(indicator.getNetValueAdded(),0)} disabled={true}/></td></tr>
+            <tr>
+              <td>Intensité (en g/€)</td>
+              <td className="column_value"><input value={printValue(indicator.getValue(),1)} disabled={true}/></td>
+              <td className="column_value"><input value={printValue(indicator.getUncertainty(),0)} disabled={true}/></td>
+            </tr>
+          </tbody>
+        </table>
+        <div>
+          <h3>Notes</h3>
+          <p>Grandeur mesurée : Quantité utilisée de produits dangereux pour la santé et/ou l’environnement (en kg)</p>
+          <p>Liste des dangers considérés :<br/>
+            - Dangereux, nocif et irritant<br/>
+            - Polluant pour l’environnement<br/>
+            - Produit dangereux pour la santé<br/>
+            - Toxique</p>
+        </div>
+      </div>
     ) 
   }
 
