0be5d74e8d355a920e38da5b710ed5dbe63647cd
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2021-06-28 13:19:09 +0200

    Add notes in GHG assessment

--- components/views/AssessmentGHG.js
+++ components/views/AssessmentGHG.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { NumberInput } from '../NumberInput';
-
+import {NumberInput} from '../NumberInput';
 
 export class AssessmentGHG extends React.Component {
 
@@ -15,73 +14,84 @@ export class AssessmentGHG extends React.Component {
     }
   }
 
-
   render() {
     const indicator = this.state.indicator;
     return (
-      <table>
-        <thead>
-          <tr><td>Emissions directes de gaz à effet de serre</td><td>Valeur</td><td>Incertitude</td></tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Emissions directes des sources fixes de combustion (en kgCO2e)</td>
-            <td className="column_value"><NumberInput value={indicator.getItem(0)}
-                              onBlur={(event) => this.updateItem(0,event.target.value)}
-                              key={this.state.versionItems}/></td>
-            <td className="column_value"><NumberInput value={indicator.getItemUncertainty(0)} 
-                              onBlur={(event) => this.updateItemUncertainty(0,event.target.value)}
-                              key={this.state.versionItems}/></td></tr>
-          <tr>
-            <td>Emissions directes des sources mobiles à moteur thermique (en kgCO2e)</td>
-            <td className="column_value"><NumberInput value={indicator.getItem(1)}
-                              onBlur={(event) => this.updateItem(1,event.target.value)}
-                              key={this.state.versionItems}/></td>
-            <td className="column_value"><NumberInput value={indicator.getItemUncertainty(1)} 
-                              onBlur={(event) => this.updateItemUncertainty(1,event.target.value)}
-                              key={this.state.versionItems}/></td></tr>
-          <tr>
-            <td>Emissions directes des procédés hors énergie (en kgCO2e)</td>
-            <td className="column_value"><NumberInput value={indicator.getItem(2)}
-                              onBlur={(event) => this.updateItem(2,event.target.value)}
-                              key={this.state.versionItems}/></td>
-            <td className="column_value"><NumberInput value={indicator.getItemUncertainty(2)} 
-                              onBlur={(event) => this.updateItemUncertainty(2,event.target.value)}
-                              key={this.state.versionItems}/></td></tr>
-          <tr>
-            <td>Emissions directes fugitives (en kgCO2e)</td>
-            <td className="column_value"><NumberInput value={indicator.getItem(3)}
-                              onBlur={(event) => this.updateItem(3,event.target.value)}
-                              key={this.state.versionItems}/></td>
-            <td className="column_value"><NumberInput value={indicator.getItemUncertainty(3)} 
-                              onBlur={(event) => this.updateItemUncertainty(3,event.target.value)}
-                              key={this.state.versionItems}/></td></tr>
-          <tr>
-            <td>Emissions issues de la biomasse - sol et forêt (en kgCO2e)</td>
-            <td className="column_value"><NumberInput value={indicator.getItem(4)}
-                              onBlur={(event) => this.updateItem(4,event.target.value)}
-                              key={this.state.versionItems}/></td>
-            <td className="column_value"><NumberInput value={indicator.getItemUncertainty(4)} 
-                              onBlur={(event) => this.updateItemUncertainty(4,event.target.value)}
-                              key={this.state.versionItems}/></td></tr>
-          <tr className="with-bottom-line">
-            <td>Total des émissions directes - SCOPE 1 (en kgCO2e)</td>
-            <td className="column_value"><NumberInput value={indicator.getTotalEmissions()}
-                              onBlur={this.updateTotalEmissions.bind(this)}
-                              ref={this.refTotal}
-                              key={this.state.versionItems}/></td>
-            <td className="column_value"><NumberInput value={printValue(indicator.getTotalEmissionsUncertainty(),0)} 
-                              onBlur={(event) => this.updateUncertainty(event.target.value)}
-                              key={this.state.versionItems}/></td></tr>
-          <tr>
-            <td>Valeur ajoutée nette (en €)</td>
-            <td className="column_value"><input value={printValue(indicator.getNetValueAdded(),0)} disabled={true}/></td></tr>
-          <tr>
-            <td>Intensité (en %)</td>
-            <td className="column_value"><input value={printValue(indicator.getValue(),1)} disabled={true}/></td>
-            <td className="column_value"><input value={printValue(indicator.getUncertainty(),0)} disabled={true}/></td></tr>
-        </tbody>
-      </table>
+      <div>
+        <table>
+          <thead>
+            <tr><td>Emissions directes de gaz à effet de serre</td><td>Valeur</td><td>Incertitude</td></tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Emissions directes des sources fixes de combustion (en kgCO2e)</td>
+              <td className="column_value"><NumberInput value={indicator.getItem(0)}
+                                onBlur={(event) => this.updateItem(0,event.target.value)}
+                                key={this.state.versionItems}/></td>
+              <td className="column_value"><NumberInput value={indicator.getItemUncertainty(0)} 
+                                onBlur={(event) => this.updateItemUncertainty(0,event.target.value)}
+                                key={this.state.versionItems}/></td></tr>
+            <tr>
+              <td>Emissions directes des sources mobiles à moteur thermique (en kgCO2e)</td>
+              <td className="column_value"><NumberInput value={indicator.getItem(1)}
+                                onBlur={(event) => this.updateItem(1,event.target.value)}
+                                key={this.state.versionItems}/></td>
+              <td className="column_value"><NumberInput value={indicator.getItemUncertainty(1)} 
+                                onBlur={(event) => this.updateItemUncertainty(1,event.target.value)}
+                                key={this.state.versionItems}/></td></tr>
+            <tr>
+              <td>Emissions directes des procédés hors énergie (en kgCO2e)</td>
+              <td className="column_value"><NumberInput value={indicator.getItem(2)}
+                                onBlur={(event) => this.updateItem(2,event.target.value)}
+                                key={this.state.versionItems}/></td>
+              <td className="column_value"><NumberInput value={indicator.getItemUncertainty(2)} 
+                                onBlur={(event) => this.updateItemUncertainty(2,event.target.value)}
+                                key={this.state.versionItems}/></td></tr>
+            <tr>
+              <td>Emissions directes fugitives (en kgCO2e)</td>
+              <td className="column_value"><NumberInput value={indicator.getItem(3)}
+                                onBlur={(event) => this.updateItem(3,event.target.value)}
+                                key={this.state.versionItems}/></td>
+              <td className="column_value"><NumberInput value={indicator.getItemUncertainty(3)} 
+                                onBlur={(event) => this.updateItemUncertainty(3,event.target.value)}
+                                key={this.state.versionItems}/></td></tr>
+            <tr>
+              <td>Emissions issues de la biomasse - sol et forêt (en kgCO2e)</td>
+              <td className="column_value"><NumberInput value={indicator.getItem(4)}
+                                onBlur={(event) => this.updateItem(4,event.target.value)}
+                                key={this.state.versionItems}/></td>
+              <td className="column_value"><NumberInput value={indicator.getItemUncertainty(4)} 
+                                onBlur={(event) => this.updateItemUncertainty(4,event.target.value)}
+                                key={this.state.versionItems}/></td></tr>
+            <tr className="with-bottom-line">
+              <td>Total des émissions directes - SCOPE 1 (en kgCO2e)</td>
+              <td className="column_value"><NumberInput value={indicator.getTotalEmissions()}
+                                onBlur={this.updateTotalEmissions.bind(this)}
+                                ref={this.refTotal}
+                                key={this.state.versionItems}/></td>
+              <td className="column_value"><NumberInput value={printValue(indicator.getTotalEmissionsUncertainty(),0)} 
+                                onBlur={(event) => this.updateUncertainty(event.target.value)}
+                                key={this.state.versionItems}/></td></tr>
+            <tr>
+              <td>Valeur ajoutée nette (en €)</td>
+              <td className="column_value"><input value={printValue(indicator.getNetValueAdded(),0)} disabled={true}/></td></tr>
+            <tr>
+              <td>Intensité (en gCO2e/€)</td>
+              <td className="column_value"><input value={printValue(indicator.getValue(),1)} disabled={true}/></td>
+              <td className="column_value"><input value={printValue(indicator.getUncertainty(),0)} disabled={true}/></td></tr>
+          </tbody>
+        </table>
+        <div>
+          <h3>Notes</h3>
+          <p>Grandeur mesurée : Emissions directes de gaz à effet de serre - SCOPE 1 (en kgCO2e)</p>
+          <p>Postes d'émissions directes :<br/>
+            - Sources fixes de combustion<br/>
+            - Sources mobiles de combustion<br/>
+            - Procédés hors énergie<br/>
+            - Emissions fugitives<br/>
+            - Biomasse</p>
+        </div>
+      </div>
     ) 
   }
 
