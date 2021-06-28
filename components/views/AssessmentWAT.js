a4bc4d8affff4175df9376f0ce04bd67f73164b8
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2021-06-28 13:21:20 +0200

    Add notes in WAT assessment

--- components/views/AssessmentWAT.js
+++ components/views/AssessmentWAT.js
@@ -15,29 +15,36 @@ export class AssessmentWAT extends React.Component {
   render() {
     const {indicator,versionItems} = this.state;
     return (
-      <table>
-        <thead>
-          <tr><td>Libelle</td><td>Valeur</td><td>Incertitude</td></tr>
-        </thead>
-        <tbody>
-          <tr className="with-bottom-line">
-              <td>Consommation totale d'eau (en m3)</td>
-              <td className="column_value"><NumberInput key={versionItems} 
-                               value={indicator.getTotalWaterConsumption()} 
-                               onBlur={this.updateTotalWaterConsumtpion.bind(this)}/></td>
-              <td className="column_value"><NumberInput key={versionItems}
-                               value={indicator.getTotalWaterConsumptionUncertainty()} 
-                               onBlur={this.updateUncertainty.bind(this)}/></td>
-          </tr><tr>
-               <td>Valeur ajoutée nette (en €)</td>
-               <td className="column_value"><input value={printValue(indicator.getNetValueAdded())} disabled={true}/></td>
-          </tr><tr>
-               <td>Intensité (en L/€)</td>
-               <td className="column_value"><input value={printValue(indicator.getValue(),1)} disabled={true}/></td>
-               <td className="column_value"><input value={printValue(indicator.getUncertainty(),0)} disabled={true}/></td>
-          </tr>
-        </tbody>
-      </table>
+      <div>
+        <table>
+          <thead>
+            <tr><td>Libelle</td><td>Valeur</td><td>Incertitude</td></tr>
+          </thead>
+          <tbody>
+            <tr className="with-bottom-line">
+                <td>Consommation totale d'eau (en m3)</td>
+                <td className="column_value"><NumberInput key={versionItems} 
+                                value={indicator.getTotalWaterConsumption()} 
+                                onBlur={this.updateTotalWaterConsumtpion.bind(this)}/></td>
+                <td className="column_value"><NumberInput key={versionItems}
+                                value={indicator.getTotalWaterConsumptionUncertainty()} 
+                                onBlur={this.updateUncertainty.bind(this)}/></td>
+            </tr><tr>
+                <td>Valeur ajoutée nette (en €)</td>
+                <td className="column_value"><input value={printValue(indicator.getNetValueAdded(),0)} disabled={true}/></td>
+            </tr><tr>
+                <td>Intensité (en L/€)</td>
+                <td className="column_value"><input value={printValue(indicator.getValue(),1)} disabled={true}/></td>
+                <td className="column_value"><input value={printValue(indicator.getUncertainty(),0)} disabled={true}/></td>
+            </tr>
+          </tbody>
+        </table>
+        <div>
+          <h3>Notes</h3>
+          <p>Grandeur mesurée : Quantité d'eau consommée (en m3)</p>
+          <p>Les prélèvements d’eau avec restitution dans le milieu d’origine, sans traitement, ne sont pas comptabilisés.</p>
+        </div>
+      </div>
     ) 
   }
 
