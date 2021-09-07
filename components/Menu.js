8dd7bc5a4a56eef19ecd094537b194e600e63e6c
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2021-09-07 18:14:18 +0200

    export menu component

--- components/Menu.js
+++ components/Menu.js
@@ -2,32 +2,53 @@
 
 import React from 'react';
 
-export class Menu extends React.Component {
+// Libraries
+import indics from '../lib/indics.json';
 
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
-    
-  render() {
-    return (
-      <div className="menu">
-        <button></button>
-      </div>
-    )
-  }
+export function Menu({selectedSection, changeSection, downloadSession, importSession}) 
+{
+
+  const triggerImportFile = () => {document.getElementById('import-session').click()};
+  const importFile = (event) => {importSession((event.target.files)[0])}
   
-}
-
-function Menu({selectedSection, parent}){
   return (
     <div className="menu">
+      <h2>MENU</h2>
       <div className="menu-items">
-        <button key={indicator}
-                onClick = {() => parent.setState({selectedSection: indicator})}
-                className={"menu-button " + ((indicator == selected) ? "selected" : "")}>
-          {indicator.toUpperCase()}
-        </button>
+        <div className="menu-items-group">
+
+          <button className={"menu-button"+("legalData"==selectedSection ? " selected" : "")}
+                  onClick = {() => changeSection("legalData")}>Unité Légale</button>
+          
+          <button className={"menu-button"+("financialData"==selectedSection ? " selected" : "")}
+                  onClick = {() => changeSection("financialData")} >Données financières</button>
+          
+          <button className={"menu-button"+("companies"==selectedSection ? " selected" : "")}
+                  onClick = {() => changeSection("companies")} >Fournisseurs</button>
+          
+          <button className={"menu-button"+("initialStates"==selectedSection ? " selected" : "")}
+                  onClick = {() => changeSection("initialStates")} >Etats initiaux</button>
+        </div>
+
+        <div className="menu-items-group">
+          {Object.entries(indics).map(([codeIndic,dataIndic]) => 
+            <button key={"menu-button-"+codeIndic}
+                    className={"menu-button"+(codeIndic==selectedSection ? " selected" : "")}
+                    onClick = {() => changeSection(codeIndic)}>
+              {dataIndic.libelleMenu}
+            </button>
+          )}
+        </div>
+        
+        <div className="menu-items-group">
+          <button onClick = {() => downloadSession()} className="menu-button">Télécharger la session</button>
+          <button onClick={triggerImportFile} className="menu-button">Importer un fichier</button>
+          <input id="import-session" type="file" accept=".json" onChange={importFile} visibility="collapse"/>
+        </div>
+        <div className="menu-items-group">
+          <button onClick={() => window.open("https://github.com/SylvainH-LSN/LaSocieteNouvelle-METRIZ-WebApp/blob/main/DOCUMENTATION.md","_blank")} className="menu-button">Documentation</button>
+          <button onClick={() => window.open("https://github.com/SylvainH-LSN/LaSocieteNouvelle-METRIZ-WebApp","_blank")} className="menu-button">Code Source</button>
+        </div>
       </div>
     </div>
   );
