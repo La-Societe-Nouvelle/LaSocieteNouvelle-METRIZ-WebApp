85346fc86b02de391c32a28ea0c5cac2529c44c5
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2021-11-05 11:21:59 +0100

    Selection A-Nouveaux oblig. pour validation import

--- components/popups/FECImportPopup.js
+++ components/popups/FECImportPopup.js
@@ -3,9 +3,6 @@
 // React
 import React from 'react';
 
-// Libraries
-import booksProps from '../../lib/books.json'
-
 /* ---------- FEC IMPORT POP-UP ---------- */
 
 export class FECImportPopup extends React.Component {
@@ -18,7 +15,7 @@ export class FECImportPopup extends React.Component {
   render() 
   {
     const {meta,books} = this.state;
-
+    
     return (
       <div className="popup">
         <div className="popup-inner">
@@ -48,21 +45,17 @@ export class FECImportPopup extends React.Component {
                     <td className="short center">{dateStart.substring(6,8)+"/"+dateStart.substring(4,6)+"/"+dateStart.substring(0,4)}</td>
                     <td className="short center">{dateEnd.substring(6,8)+"/"+dateEnd.substring(4,6)+"/"+dateEnd.substring(0,4)}</td>
                     <td className="short center">{nLines}</td>
-                    {/*<td className="medium center">
-                      <select onChange={(event) => this.onBookTypeChange(code,event)} value={type}>
-                        {Object.entries(booksProps).map(([type,{label}]) => <option key={type} value={type}>{label}</option>)}
-                        <option key={"AUTRE"} value={"AUTRE"}>{"---"}</option>
-                      </select></td>*/}
-                  <td>
-                    <input type="radio" name="ANOUVEAUX" value={code} checked={type=="ANOUVEAUX"} onChange={this.changeJournalANouveaux}/>
-                  </td>
+                    <td>
+                      <input type="radio" name="ANOUVEAUX" value={code} checked={type=="ANOUVEAUX"} onChange={this.changeJournalANouveaux}/>
+                    </td>
                   </tr>
                 )}
               )}
             </tbody>
           </table>
           <div className="footer">
-            <button onClick={() => this.validate()}>Valider</button>
+            <button onClick={() => this.validate()}
+                    disabled={!(Object.entries(meta.books).filter(([_,{type}]) => type=="ANOUVEAUX").length > 0)}>Valider</button>
           </div>
         </div>
       </div>
