d100a7d228f1ebeb42aac582e2f9422206553f71
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2021-11-26 21:08:54 +0100

    Mise à jour - document publication

--- src/writers/StatementWriter.js
+++ src/writers/StatementWriter.js
@@ -53,12 +53,12 @@ const writeStatementPDF = (data) =>
   y+=15;
   doc.setFontSize(11);
   doc.setFont("Calibri","bold");
-  doc.text("Données publiées :",10,y);
+  doc.text("Données publiées",10,y);
   doc.line(10,y+2,200,y+2);
 
   /* ----- TABLE ----- */
 
-  let xNote = 140;
+  let xNote = 145;
   let xValue = 160;
   let xUncertainty = 180;
 
@@ -75,27 +75,17 @@ const writeStatementPDF = (data) =>
                                       .forEach(([indic,indicator]) => 
   {
     y+=6;
-    doc.setFont("Calibri","bold");
     doc.text(metaIndics[indic].libelle,10,y);
 
-    doc.setFont("Calibri","normal");
     if (data.comments[indic]) {
       notes.push(data.comments[indic]);
       doc.text(""+notes.length,xNote,y,{align: "center"});
     }
     doc.text(printValue(indicator.value,0)+" ",xValue+7,y,{align: "right"});
-    doc.text(metaIndics[indic].unit,xValue+7,y,{align: "left"});
     doc.setFontSize(8);
+    doc.text(metaIndics[indic].unit,xValue+7,y,{align: "left"});
     doc.text(printValue(indicator.uncertainty,0)+" %",xUncertainty+12,y,{align: "right"});
     doc.setFontSize(10);
-    if (indicator.info.length > 0) {
-      y+=6;
-      doc.setFontSize(8);
-      let infos = doc.splitTextToSize("NOTE : "+indicator.info,100);
-      doc.text(infos,15,y);
-      doc.setFontSize(10);
-      y+=(infos.length-1)*4;
-    }
   })
   if (Object.entries(data.socialFootprint).filter(([_,indicator]) => indicator.value != null).length == 0) {
     y+=6;
@@ -111,6 +101,14 @@ const writeStatementPDF = (data) =>
   doc.text("Déclaration faite par : "+(data.declarant || ""),10,y); 
 
   y+=20;
+  doc.setFontSize(11);
+  doc.setFont("Calibri","bold");
+  doc.text("Informations relatives à la publication",10,y);
+  doc.line(10,y+2,200,y+2);
+  
+  doc.setFontSize(10);
+  doc.setFont("Calibri","normal");
+  y+=10;
   doc.text("Le coût de la formalité est de "+(data.price || " - ")+" €",10,y)
   y+=6;
   doc.text("Le délai de publication des données est de 7 jours. Un mail de confirmation vous sera envoyé.",10,y)
@@ -124,7 +122,7 @@ const writeStatementPDF = (data) =>
   if (notes.length > 0) 
   {
     doc.addPage();
-    y = 10;
+    y = 15;
 
     // Titre
     doc.setFontSize(14);
@@ -136,11 +134,14 @@ const writeStatementPDF = (data) =>
     doc.setFont("Calibri","normal");
     for (let i = 0; i < notes.length; i++)
     {
-      doc.text(""+(i+1),10,y);
-      let lignesNote = doc.splitTextToSize(notes[i],100);
+      doc.text(""+(i+1),15,y);
+      let lignesNote = doc.splitTextToSize(notes[i],170);
       doc.text(lignesNote,20,y);
       y+=lignesNote*6;
     }
+
+    // Base de page
+    doc.text("La Société Nouvelle - Société immatriculée au RCS de Lille Métropole - 889 182 770",105,280,{align: "center"})
   }
 
   // ----- RETURN ----- //
