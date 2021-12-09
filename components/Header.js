9d9b9705de40c1fdb0097897d6bfbca3526a65a9
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2021-12-09 11:06:10 +0100

    Update url to last doc version

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ export function Header({step,stepMax,downloadSession,setStep})
 {  
   const refresh = () => location.reload(true);
   const openRepository = () => window.open("https://github.com/SylvainH-LSN/LaSocieteNouvelle-METRIZ-WebApp","_blank");
-  const openDocumentation = () => window.open("https://lasocietenouvelle.org/METRIZ_documentation_v1.pdf","_blank");
+  const openDocumentation = () => window.open("https://lasocietenouvelle.org/METRIZ_documentation_v1.1.3.pdf","_blank");
   const redirectToContactForm = () => window.open("https://lasocietenouvelle.org/contact","_blank");
   const saveSession = () => downloadSession();
   const goBack = () => setStep(5);
