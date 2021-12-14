a49f8bc98f88aeb9e63785442b802bd48cf4340d
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2021-12-14 11:23:03 +0100

    Upgrade analysis eco

--- src/writers/analysis/analysisTextWriterECO.js
+++ src/writers/analysis/analysisTextWriterECO.js
@@ -5,12 +5,22 @@ import { printValue } from "../../utils/Utils";
 export const analysisTextWriterECO = (session) =>
 {
   const {impactsData,
+         legalUnit,
          financialData} = session;
-  const {aggregates} = financialData;
+  const {aggregates,
+         expenseAccounts} = financialData;
 
   // array of paragraphs
   let analysis = [];
   let currentParagraph = [];
+
+  // Intro ----------------------------------------------------------------------------------------- //
+
+  currentParagraph = [];
+  
+  currentParagraph.push("L'indicateur renseigne sur la part de la valeur produite en France.");
+  
+  analysis.push(currentParagraph);
 
   // Impact direct --------------------------------------------------------------------------------- //
 
@@ -36,28 +46,39 @@ export const analysisTextWriterECO = (session) =>
   analysis.push(currentParagraph);
 
   // comparaison branche
-  /*
-  if (aggregates.intermediateConsumption.footprint.indicators.eco.getValue() > 50) {
-    currentParagraph.push("Le taux de contribution se situe à un niveau supérieur à la branche d'activités ("+branche+").")
-  } else {
-    currentParagraph.push("Le taux de contribution se situe à un niveau inférieur à la branche d'activités ("+branche+").")
+  
+  if (legalUnit.consumptionSectorFootprint.indicators.eco.getValue()!=null)
+  {
+    if (aggregates.intermediateConsumption.footprint.indicators.eco.getValue() > legalUnit.consumptionSectorFootprint.indicators.eco.getValue()*0.9
+     && aggregates.intermediateConsumption.footprint.indicators.eco.getValue() < legalUnit.consumptionSectorFootprint.indicators.eco.getValue()*1.1) {
+      currentParagraph.push("Le taux de contribution se situe à un niveau similaire à la branche d'activités.")
+    }
+    if (aggregates.intermediateConsumption.footprint.indicators.eco.getValue() > legalUnit.consumptionSectorFootprint.indicators.eco.getValue()) {
+      currentParagraph.push("Le taux de contribution se situe à un niveau supérieur à la branche d'activités.")
+    } else {
+      currentParagraph.push("Le taux de contribution se situe à un niveau inférieur à la branche d'activités.")
+    }
   }
-  */
 
   // comptes les plus impactants
-  //let maxCompany = financialData.accounts.sort((a,b) => b.amount - a.amount)[0];
-  //currentParagraph.push("Les charges "+maxCompany+" impactent le plus négativement la performance de l'entreprise, avec un taux de contribution de "+""+" pour un volume de "+""+" €.");
+  let bestAccount = expenseAccounts.sort((a,b) => b.footprint.indicators.eco.getGrossImpact(b.amount) - a.footprint.indicators.eco.getGrossImpact(a.amount))[0];
+  console.log(bestAccount);
+  currentParagraph.push("Le compte de charges \""+bestAccount.accountLib.charAt(0)+bestAccount.accountLib.substring(1).toLowerCase()+"\" représente la plus grosse contribution indirecte de l'entreprise, avec un taux de contribution de "+bestAccount.footprint.indicators.eco.getValue()+" % pour un volume de "+printValue(bestAccount.amount,0)+" €.");
 
   // Investissements ------------------------------------------------------------------------------- //
 
-  // 
-  //currentParagraph.push("La contribution des immobilisations de l'entreprise est de "+"");
+  currentParagraph = [];
+
+  currentParagraph.push("L'amortissement des immobilisations apportent une contribution indirecte de "+printValue(aggregates.capitalConsumption.footprint.indicators.eco.getGrossImpact(aggregates.capitalConsumption.amount),0)+" €, soit "+aggregates.capitalConsumption.footprint.indicators.eco.getValue()+" % du montant des dotations aux amortissements.");
+
+  analysis.push(currentParagraph);
+
 
   // Production ------------------------------------------------------------------------------------ //
   
   currentParagraph = [];
 
-  currentParagraph.push("Le taux de contribution de la production sur l'exercice est de "+aggregates.production.footprint.indicators.eco.getValue()+" %.")
+  currentParagraph.push("In fine, le taux de contribution de la production sur l'exercice est de "+aggregates.production.footprint.indicators.eco.getValue()+" %.")
   if (aggregates.production.footprint.indicators.eco.getValue()!=aggregates.revenue.footprint.indicators.eco.getValue()) {
     currentParagraph.push("La valeur est de "+aggregates.revenue.footprint.indicators.eco.getValue()+"% pour le chiffre d'affaires, en prenant compte des stocks de production.")
   } else {
