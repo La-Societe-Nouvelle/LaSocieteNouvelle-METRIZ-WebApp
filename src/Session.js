87ae594b6db318234a011198d63c19f372d972ff
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2022-01-16 22:50:03 +0100

    configure updater for 1.0.0

--- src/Session.js
+++ src/Session.js
@@ -1,6 +1,6 @@
 // La Société Nouvelle
 
-const currentVersion = "1.0.0";
+const currentVersion = "1.0.1";
 
 // Libraries
 import metaIndics from '../lib/indics.json';
--- src/version/updateVersion.js
+++ src/version/updateVersion.js
@@ -6,10 +6,24 @@
 
 export const updateVersion = (sessionData) =>
 {
-  /*
   switch(session.version)
   {
-    case "1.0.1": //process
+    case "1.0.1": break;
+    case "1.0.0": updater_1_0_0(sessionData); break;
+    default : updater_1_0_0(sessionData);
   }
-  */
+}
+
+const updater_1_0_0 = (sessionData) =>
+{
+  // update ghgDetails
+  sessionData.impactsData.ghgDetails.forEach((item) =>
+  {
+    // update name factor id
+    item.factorId = item.fuelCode;
+    // update prefix id
+    if (/^p/.test(item.factorId)) item.factorId = "fuel"+item.factorId.substring(1);
+    if (/^s/.test(item.factorId)) item.factorId = "coolSys"+item.factorId.substring(1);
+    if (/^industrialProcesses_/.test(item.factorId)) item.factorId = "indusProcess"+item.factorId.substring(20);
+  })
 }
