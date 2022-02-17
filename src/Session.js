bb59d46334c34e4b16228d2233d2ed19396157cd
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2022-02-17 11:56:16 +0100

    increase version - variable name error ghgDetails

--- src/Session.js
+++ src/Session.js
@@ -1,6 +1,6 @@
 // La Société Nouvelle
 
-const currentVersion = "1.0.1";
+const currentVersion = "1.0.2";
 
 // Libraries
 import metaIndics from '../lib/indics.json';
--- src/version/updateVersion.js
+++ src/version/updateVersion.js
@@ -1,5 +1,7 @@
 // La Société Nouvelle
 
+import { getGhgEmissionsUncertainty, getTotalGhgEmissionsUncertainty } from "../../components/assessments/AssessmentGHG";
+
 /* ----------------------------------------------------------------- */
 /* -------------------- MANAGE PREVIOUS VERSION -------------------- */
 /* ----------------------------------------------------------------- */
@@ -8,8 +10,11 @@ export const updateVersion = (sessionData) =>
 {
   switch(sessionData.version)
   {
-    case "1.0.1": break;
-    case "1.0.0": updater_1_0_0(sessionData); break;
+    case "1.0.2": break;
+
+    case "1.0.0": updater_1_0_0(sessionData);
+    case "1.0.1": updater_1_0_1(sessionData); break;
+    
     default : updater_1_0_0(sessionData);
   }
 }
@@ -26,4 +31,16 @@ const updater_1_0_0 = (sessionData) =>
     if (/^s/.test(itemData.factorId)) itemData.factorId = "coolSys"+itemData.factorId.substring(1);
     if (/^industrialProcesses_/.test(itemData.factorId)) itemData.factorId = "indusProcess"+itemData.factorId.substring(20);
   })
+}
+
+const updater_1_0_1 = (sessionData) =>
+{
+  // update ghgDetails (error with variable name in NRG tool : getGhgEmissionsUncertainty used instead of ghgEmissionsUncertainty)
+  Object.entries(sessionData.impactsData.ghgDetails).forEach(([_,itemData]) =>
+  {
+    // update name
+    itemData.ghgEmissionsUncertainty = getGhgEmissionsUncertainty(itemData);
+  })
+  // update value
+  sessionData.impactsData.ghgEmissionsUncertainty = getTotalGhgEmissionsUncertainty(sessionData.impactsData.ghgDetails);
 }
