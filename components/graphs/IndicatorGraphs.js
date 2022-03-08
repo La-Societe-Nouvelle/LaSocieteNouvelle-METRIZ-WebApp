a79fe712f5827d42695c5b1d01e7b7c7ba13eb99
Laura Roost <laura.roost@lasocietenouvelle.org>
2022-03-08 14:42:32 +0100

    Null data in graph and table hidden

--- components/graphs/IndicatorGraphs.js
+++ components/graphs/IndicatorGraphs.js
@@ -30,6 +30,46 @@ const viewsForIndic = {
 }
 
 export const IndicatorGraphs = ({ session, indic, comparativeFootprints }) => {
+
+
+
+
+  const { financialData } = session;
+  const { production, netValueAdded, intermediateConsumption } = financialData.aggregates;
+  const { productionSectorFootprint, valueAddedSectorFootprint, consumptionSectorFootprint, allSectorsProductionAreaFootprint, allSectorsValueAddedAreaFootprint, allSectorsConsumptionFootprint } = comparativeFootprints;
+
+  // PRODUCTION CHART
+
+  const labelsP = ['France', ['Exercice', 'en cours'], 'Branche'];
+
+  const dataP = [
+    allSectorsProductionAreaFootprint.getIndicator(indic).value,
+    production.footprint.getIndicator(indic).value,
+    productionSectorFootprint.getIndicator(indic).value]
+    ;
+
+  for (let i = 0; i < dataP.length; i++) {
+
+    if (dataP[i] === null) {
+
+      dataP.splice(i, 1);
+      labelsP.splice(i, 1);
+    }
+
+  }
+
+  const dataProduction = {
+    labels: labelsP,
+    datasets: [
+      {
+        label: 'Production',
+        data: dataP,
+        backgroundColor: ["RGB(219, 222, 241)", "RGB(251, 122, 127)", "RGB(219, 222, 241)"],
+      },
+    ],
+  };
+
+
   const optionsP = {
     responsive: true,
     maintainAspectRatio: false,
@@ -38,7 +78,7 @@ export const IndicatorGraphs = ({ session, indic, comparativeFootprints }) => {
       x: {
         grid: {
           display: false
-        }
+        },
       },
       y:
       {
@@ -54,7 +94,8 @@ export const IndicatorGraphs = ({ session, indic, comparativeFootprints }) => {
               weight: 'bold'
             }
           },
-        }
+        },
+
       },
       legend: {
         display: false,
@@ -79,6 +120,39 @@ export const IndicatorGraphs = ({ session, indic, comparativeFootprints }) => {
     },
 
   };
+
+  // CONSUMPTION CHART 
+
+
+  const labelsC = ['France', ['Exercice', 'en cours'], 'Branche'];
+  const dataC = [
+    allSectorsConsumptionFootprint.getIndicator(indic).value,
+    intermediateConsumption.footprint.getIndicator(indic).value,
+    consumptionSectorFootprint.getIndicator(indic).value,
+  ]
+
+
+  for (let i = 0; i < dataC.length; i++) {
+
+    if (dataC[i] === null) {
+
+      dataC.splice(i, 1);
+      labelsC.splice(i, 1);
+    }
+
+  }
+
+  const dataConsumption = {
+    labels: labelsC,
+    datasets: [
+      {
+        label: 'Consommation',
+        data: dataC,
+        backgroundColor: ["RGB(219, 222, 241)", "RGB(251, 122, 127)", "RGB(219, 222, 241)"],
+      },
+    ],
+  };
+
 
   const optionsC = {
     responsive: true,
@@ -132,13 +206,44 @@ export const IndicatorGraphs = ({ session, indic, comparativeFootprints }) => {
         display: true,
         align: "start",
         position: "top",
-        text: "Consommation intermédiaire",
+        text: "Consommations intermédiaires",
       },
 
     },
 
   };
 
+  // VALUE CHART
+  const labelsV = ['France', ['Exercice', 'en cours'], 'Branche'];
+
+  // To Do : round value Number().toFixed(2); // 1.00
+  const dataV = [
+    allSectorsValueAddedAreaFootprint.getIndicator(indic).value,
+    valueAddedSectorFootprint.getIndicator(indic).value,
+    netValueAdded.footprint.getIndicator(indic).value,
+  ];
+
+  for (let i = 0; i < dataV.length; i++) {
+
+    if (dataV[i] === null) {
+
+      dataV.splice(i, 1);
+      labelsV.splice(i, 1);
+    }
+
+  }
+
+  const dataValueAdded = {
+    labels: labelsV,
+    datasets: [
+      {
+        label: 'Valeur ajoutée',
+        data: dataV,
+        backgroundColor: ["RGB(219, 222, 241)", "RGB(251, 122, 127)", "RGB(219, 222, 241)"],
+      },
+
+    ],
+  };
 
   const optionsV = {
     responsive: true,
@@ -152,7 +257,7 @@ export const IndicatorGraphs = ({ session, indic, comparativeFootprints }) => {
       },
       y:
       {
-        display: false, 
+        display: false,
       },
     },
     plugins: {
@@ -188,44 +293,7 @@ export const IndicatorGraphs = ({ session, indic, comparativeFootprints }) => {
     },
 
   };
-  const labels = [['Exercice', 'en cours'], 'Branche', 'France'];
 
-  const { financialData } = session;
-  const { production, netValueAdded, intermediateConsumption } = financialData.aggregates;
-  const { productionSectorFootprint, valueAddedSectorFootprint, consumptionSectorFootprint, productionAreaFootprint, valueAddedAreaFootprint } = comparativeFootprints;
-
-
-  const dataProduction = {
-    labels,
-    datasets: [
-      {
-        label: 'Production',
-        data: [production.footprint.getIndicator(indic).value, productionSectorFootprint.getIndicator(indic).value, productionAreaFootprint.getIndicator(indic).value],
-        backgroundColor: ["RGB(251, 122, 127)", "RGB(219, 222, 241)", "RGB(219, 222, 241)"],
-      },
-    ],
-  };
-
-  const dataValueAdded = {
-    labels,
-    datasets: [
-      {
-        label: 'Valeur ajoutée',
-        data: [netValueAdded.footprint.getIndicator(indic).value, valueAddedSectorFootprint.getIndicator(indic).value, valueAddedAreaFootprint.getIndicator(indic).value],
-        backgroundColor: ["RGB(251, 122, 127)", "RGB(219, 222, 241)", "RGB(219, 222, 241)"],
-      },
-    ],
-  };
-  const dataConsumption = {
-    labels,
-    datasets: [
-      {
-        label: 'Consommation',
-        data: [intermediateConsumption.footprint.getIndicator(indic).value, consumptionSectorFootprint.getIndicator(indic).value],
-        backgroundColor: ["RGB(251, 122, 127)", "RGB(219, 222, 241)", "RGB(219, 222, 241)"],
-      },
-    ],
-  };
   const unit = metaIndics[indic].unit;
   const viewWindow = viewsForIndic[indic];
 
@@ -257,42 +325,52 @@ export const IndicatorGraphs = ({ session, indic, comparativeFootprints }) => {
         <ColumnChart title="titre" data={dataValueAdded} viewWindow={viewWindow} title="Valeur Ajoutée"/>  */}
         </div>
       </div>
+
       <table>
         <thead>
           <tr>
             <td className="auto" colSpan="1">Agrégat</td>
-            <td className="column_value" colSpan="2">Unité légale</td>
-            <td className="column_value" colSpan="2">Branche</td>
-            <td className="column_value" colSpan="2">France</td>
+            <td className="column_value" >France</td>
+            <td className="column_value align-center" >Exercice en cours</td>
+            {
+              printValue(productionSectorFootprint.getIndicator(indic).value, 1)
+                && printValue(consumptionSectorFootprint.getIndicator(indic).value, 1)
+                && printValue(valueAddedSectorFootprint.getIndicator(indic).value, 1) !== " - " ?
+                
+                <td className="column_value" >Branche</td> : ""
+            }
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>Production</td>
-            <td className="short right">{printValue(production.footprint.getIndicator(indic).value, 1)}</td>
-            <td className="column_unit">&nbsp;{unit}</td>
-            <td className="short right">{printValue(productionSectorFootprint.getIndicator(indic).value, 1)}</td>
-            <td className="column_unit">&nbsp;{unit}</td>
-            <td className="short right">{printValue(productionAreaFootprint.getIndicator(indic).value, 1)}</td>
-            <td className="column_unit">&nbsp;{unit}</td>
+            <td className="short right">{printValue(allSectorsProductionAreaFootprint.getIndicator(indic).value, 1)} {unit}</td>
+            <td className="short align-center">{printValue(production.footprint.getIndicator(indic).value, 1)} {unit}</td>
+            {
+               printValue(productionSectorFootprint.getIndicator(indic).value, 1) !== " - " ?
+               <td className="short right">{printValue(productionSectorFootprint.getIndicator(indic).value, 1)} {unit}</td>
+               : ""
+            }
           </tr>
           <tr>
             <td>Consommations intermédiaires</td>
-            <td className="short right">{printValue(intermediateConsumption.footprint.getIndicator(indic).value, 1)}</td>
-            <td className="column_unit">&nbsp;{unit}</td>
-            <td className="short right">{printValue(consumptionSectorFootprint.getIndicator(indic).value, 1)}</td>
-            <td className="column_unit">&nbsp;{unit}</td>
-            <td className="short right">{printValue(null, 1)}</td>
-            <td className="column_unit">&nbsp;{unit}</td>
+            <td className="short right">{printValue(allSectorsConsumptionFootprint.getIndicator(indic).value, 1)} {unit}</td>
+            <td className="short align-center">{printValue(intermediateConsumption.footprint.getIndicator(indic).value, 1)} {unit}</td>
+            {
+              printValue(consumptionSectorFootprint.getIndicator(indic).value, 1) !== " - " ? 
+              <td className="short right">{printValue(consumptionSectorFootprint.getIndicator(indic).value, 1)} {unit}</td>
+              : ""
+            }
           </tr>
           <tr>
             <td>Valeur ajoutée</td>
-            <td className="short right">{printValue(netValueAdded.footprint.getIndicator(indic).value, 1)}</td>
-            <td className="column_unit">&nbsp;{unit}</td>
-            <td className="short right">{printValue(valueAddedSectorFootprint.getIndicator(indic).value, 1)}</td>
-            <td className="column_unit">&nbsp;{unit}</td>
-            <td className="short right">{printValue(valueAddedAreaFootprint.getIndicator(indic).value, 1)}</td>
-            <td className="column_unit">&nbsp;{unit}</td>
+            <td className="short right">{printValue(allSectorsValueAddedAreaFootprint.getIndicator(indic).value, 1)} {unit}</td>
+            <td className="short align-center">{printValue(netValueAdded.footprint.getIndicator(indic).value, 1)} {unit}</td>
+            {
+              printValue(valueAddedSectorFootprint.getIndicator(indic).value, 1) !== " - " ?
+              <td className="short right">{printValue(valueAddedSectorFootprint.getIndicator(indic).value, 1)} {unit}</td> 
+              : ""
+            }
           </tr>
         </tbody>
       </table>
