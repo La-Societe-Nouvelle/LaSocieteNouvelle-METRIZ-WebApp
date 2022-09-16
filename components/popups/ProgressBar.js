62a2ce7631a93eedcc3b2777301f0942ae74fde0
Laura Roost <laura.roost@lasocietenouvelle.org>
2022-09-16 12:24:57 +0200

    add progress bar

--- components/popups/ProgressBar.js
+++ components/popups/ProgressBar.js
@@ -2,25 +2,26 @@
 
 // React
 import React from "react";
+import { Modal } from "react-bootstrap";
 
 /* ---------- FEC IMPORT POP-UP ---------- */
 
 export const ProgressBar = ({ message, progression }) => {
   return (
-    <div className="modal-overlay">
-      <div className="modal-wrapper">
-        <div className="modal">
-          <div className="body">
-            <h3>{message}</h3>
-            <div className="progressbar">
-              <svg className="progressbar_shape">
-                <rect width={progression + "%"} />
-              </svg>
-            </div>
-          </div>
+    <Modal show="true" size="md" centered>
+      <Modal.Header>
+        <Modal.Title>{message}</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <div className="progressbar">
+          <div
+            className="progression"
+            style={{ width: `${progression}%` }}
+          ></div>
         </div>
-      </div>
-    </div>
+      </Modal.Body>
+    </Modal>
   );
   // return (
   // <div className="popup-container">
