dae3993e3a72ba81bcd082f4abe87d46267f56a7
Laura Roost <laura.roost@lasocietenouvelle.org>
2022-10-14 16:43:13 +0200

    import charts config

--- components/graphs/GraphsPDF.js
+++ components/graphs/GraphsPDF.js
@@ -7,6 +7,10 @@ import { Col, Row } from "react-bootstrap";
 Chart.register(ChartDataLabels);
 import { Bar } from "react-chartjs-2";
 import PieGraph from "./PieGraph";
+import {
+  ComparativeGraphsData,
+  ComparativeGraphsOptions,
+} from "./ComparativeGraphs.config";
 
 // Libraries
 import metaIndics from "/lib/indics";
@@ -37,372 +41,96 @@ export const GraphsPDF = ({
     consumptionDivisionFootprint,
   } = comparativeDivisionFootprint;
 
-  console.log(allSectorFootprint)
   const printGrossImpact = ["ghg", "haz", "mat", "nrg", "was", "wat"].includes(
     indic
   );
   const unit = metaIndics[indic].unit;
+  const precision = metaIndics[indic].nbDecimals;
 
-  // TARGET SNBC VALUE
-  let valueAddedTargetData;
-  let productionTargetData;
-  let consumptionTargetData;
+  // PRODUCTION CHART
+  const labels = ["France", "Exercice en cours"];
 
-  if (targetSNBC) {
-    productionTargetData = [
-      targetSNBC.productionTarget.value,
-      targetSNBC.productionTarget.value,
-      targetSNBC.productionTarget.value,
-    ];
-    consumptionTargetData = [
-      targetSNBC.consumptionTarget.value,
-      targetSNBC.consumptionTarget.value,
-      targetSNBC.consumptionTarget.value,
-    ];
-    valueAddedTargetData = [
-      targetSNBC.valueAddedTarget.value,
-      targetSNBC.valueAddedTarget.value,
-      targetSNBC.valueAddedTarget.value,
-    ];
+  if (comparativeDivisionFootprint) {
+    labels.push("Branche");
   }
-  // // PRODUCTION CHART
-  const labelsP = ["France", ["Exercice", "en cours"], "Branche"];
+
   const dataP = [
-    roundNumber(productionAreaFootprint.value),
-    roundNumber(production.footprint.getIndicator(indic).value),
-    roundNumber(productionDivisionFootprint.value),
+    productionAreaFootprint.value.toFixed(precision),
+    production.footprint.getIndicator(indic).value.toFixed(precision),
   ];
 
-  for (let i = 0; i < dataP.length; i++) {
-    if (dataP[i] === null) {
-      dataP.splice(i, 1);
-      labelsP.splice(i, 1);
-    }
+  let dataComparativeP;
+
+  if (productionDivisionFootprint) {
+    dataP.push(productionDivisionFootprint.value.toFixed(precision));
+  }
+  if (targetSNBC) {
+    dataComparativeP = targetSNBC.productionTarget.value.toFixed(precision);
   }
 
-  const dataProduction = {
-    labels: labelsP,
-    datasets: [
-      {
-        label: "Production",
-        data: dataP,
-        backgroundColor: [
-          "RGB(219, 222, 241)",
-          "RGB(251, 122, 127)",
-          "RGB(176, 185, 247)",
-        ],
-        borderWidth: 1,
-        borderColor: [
-          "RGB(202, 209, 240)",
-          "RGB(241, 97, 103)",
-          "RGB(146, 159, 249)",
-        ],
-        barPercentage: 1,
-        barThickness: 50,
-        type: "bar",
-        yAxisID: "y2",
-        xAxisID: "x2",
-      },
-    ],
-  };
-  if (productionTargetData) {
-    dataProduction.datasets.push({
-      label: "Trajectoire SNBC",
-      data: productionTargetData,
-      type: "line",
-      yAxisID: "y1",
-      xAxisID: "x1",
-      backgroundColor: "RGBA(255, 152, 7,0.4)",
-      borderWidth: 1,
-      borderColor: "RGB(255, 152, 7)",
-      pointRadius: [0, 0, 5],
-    });
-  }
+  const productionDataConfig = ComparativeGraphsData(
+    "Production",
+    dataP,
+    dataComparativeP
+  );
+  const productionOptionConfig = ComparativeGraphsOptions(
+    labels,
+    unit
+  );
 
-  const optionsP = {
-    scales: {
-      x2: {
-        type: "category",
-        labels: labelsP,
-        ticks: {
-          color: "#212529",
-        },
-      },
-      x1: {
-        labels: labelsP,
-        display: false,
-        offset: false,
-      },
-      y1: {
-        display: false,
-        type: "linear",
-        position: "left",
-        ticks: {
-          max: 100,
-          min: 0,
-        },
-      },
-      y2: {
-        display: true,
-        title: {
-          display: true,
-          text: unit,
-          color: "#212529",
-        },
-        ticks: {
-          color: "#212529",
-        },
-      },
-    },
-    plugins: {
-      datalabels: {
-        labels: {
-          display: false,
-        },
-      },
-      title: {
-        padding: {
-          top: 0,
-          bottom: 25,
-        },
-        font: {
-          size: 18,
-          weight: "bold",
-        },
-        display: true,
-        align: "center",
-        position: "top",
-        color: "#191558",
-        text: "Production",
-      },
-    },
-  };
+// CONSUMPTION CHART
 
-  // CONSUMPTION CHART
+const dataC = [
+  consumptionAreaFootprint.value.toFixed(precision),
+  intermediateConsumption.footprint
+    .getIndicator(indic)
+    .value.toFixed(precision),
+];
+if (consumptionDivisionFootprint) {
+  dataC.push(consumptionDivisionFootprint.value.toFixed(precision));
+}
+let dataComparativeC;
 
-  const labelsC = ["France", ["Exercice", "en cours"], "Branche"];
+if (targetSNBC) {
+  dataComparativeC = targetSNBC.consumptionTarget.value.toFixed(precision);
+}
 
-  const dataC = [
-    roundNumber(consumptionAreaFootprint.value),
-    roundNumber(intermediateConsumption.footprint.getIndicator(indic).value),
-    roundNumber(consumptionDivisionFootprint.value),
-  ];
+const consumptionDataConfig = ComparativeGraphsData(
+  "Consommation",
+  dataC,
+  dataComparativeC
+);
+const consumptionOptionConfig = ComparativeGraphsOptions(
+  labels,
+  unit
+);
 
-  for (let i = 0; i < dataC.length; i++) {
-    if (dataC[i] === null) {
-      dataC.splice(i, 1);
-      labelsC.splice(i, 1);
-    }
-  }
+// VALUE CHART
+let dataComparativeV;
+ 
+const dataV = [
+  valueAddedAreaFootprint.value.toFixed(precision),
+  netValueAdded.footprint.getIndicator(indic).value.toFixed(precision),
+];
+if (valueAddedDivisionFootprint) {
+  dataV.push(valueAddedDivisionFootprint.value.toFixed(precision));
+}
 
-  const dataConsumption = {
-    labels: labelsC,
-    datasets: [
-      {
-        label: "Trajectoire SNBC",
-        data: consumptionTargetData,
-        type: "line",
-        yAxisID: "y1",
-        xAxisID: "x1",
-        backgroundColor: "RGBA(255, 152, 7,0.4)",
-        borderWidth: 1,
-        borderColor: "RGBA(255, 152, 7, 0.7)",
-        pointRadius: [0, 0, 5],
-      },
-      {
-        label: "Consommation",
-        data: dataC,
-        backgroundColor: [
-          "RGB(219, 222, 241)",
-          "RGB(251, 122, 127)",
-          "RGB(176, 185, 247)",
-        ],
-        borderWidth: 1,
-        borderColor: [
-          "RGB(202, 209, 240)",
-          "RGB(241, 97, 103)",
-          "RGB(146, 159, 249)",
-        ],
-        barPercentage: 1,
-        barThickness: 50,
-        type: "bar",
-        yAxisID: "y2",
-        xAxisID: "x2",
-      },
-    ],
-  };
+if (targetSNBC) {
+  dataComparativeV = targetSNBC.valueAddedTarget.value.toFixed(precision);
+}
 
-  const optionsC = {
-    scales: {
-      x2: {
-        type: "category",
-        labels: labelsC,
-        ticks: {
-          color: "#212529",
-        },
-      },
-      x1: {
-        labels: labelsC,
-        display: false,
-        offset: false,
-      },
-      y1: {
-        display: false,
-        type: "linear",
-        position: "left",
-        ticks: {
-          max: 100,
-          min: 0,
-        },
-      },
-      y2: {
-        display: true,
-        title: {
-          display: true,
-          text: unit,
-          color: "#212529",
-        },
-        ticks: {
-          color: "#212529",
-        },
-      },
-    },
-    plugins: {
-      datalabels: {
-        labels: {
-          display: false,
-        },
-      },
-      title: {
-        padding: {
-          top: 0,
-          bottom: 25,
-        },
-        font: {
-          size: 18,
-          weight: "bold",
-        },
-        display: true,
-        align: "center",
-        position: "top",
-        color: "#191558",
-        text: "Consommations intermédiaires",
-      },
-    },
-  };
+const dataValueAddedConfig = ComparativeGraphsData(
+  labels,
+  dataV,
+  dataComparativeV
+);
+const ValueAddedOptionConfig = ComparativeGraphsOptions(
+  labels,
+  unit
+);
 
-  // VALUE CHART
-  const labelsV = ["France", ["Exercice", "en cours"], "Branche"];
-console.log(valueAddedAreaFootprint)
-  // To Do : round value Number().toFixed(2); // 1.00
-  const dataV = [
-    roundNumber(valueAddedAreaFootprint.value),
-    roundNumber(netValueAdded.footprint.getIndicator(indic).value),
-    roundNumber(valueAddedDivisionFootprint.value),
-  ];
 
-  for (let i = 0; i < dataV.length; i++) {
-    if (dataV[i] === null) {
-      dataV.splice(i, 1);
-      labelsV.splice(i, 1);
-    }
-  }
-
-  const dataValueAdded = {
-    labels: labelsV,
-    datasets: [
-      {
-        label: "Trajectoire SNBC",
-        data: valueAddedTargetData,
-        type: "line",
-        yAxisID: "y1",
-        xAxisID: "x1",
-        backgroundColor: "RGBA(255, 152, 7,0.4)",
-        borderWidth: 1,
-        borderColor: "RGB(255, 152, 7)",
-        pointRadius: [0, 0, 5],
-      },
-      {
-        label: "Valeur ajoutée",
-        data: dataV,
-        backgroundColor: [
-          "RGB(219, 222, 241)",
-          "RGB(251, 122, 127)",
-          "RGB(176, 185, 247)",
-        ],
-        borderWidth: 1,
-        borderColor: [
-          "RGB(202, 209, 240)",
-          "RGB(241, 97, 103)",
-          "RGB(146, 159, 249)",
-        ],
-        barPercentage: 1,
-        barThickness: 50,
-        type: "bar",
-        yAxisID: "y2",
-        xAxisID: "x2",
-      },
-    ],
-  };
-
-  const optionsV = {
-    scales: {
-      x2: {
-        type: "category",
-        labels: labelsV,
-        ticks: {
-          color: "#212529",
-        },
-      },
-      x1: {
-        labels: labelsV,
-        display: false,
-        offset: false,
-      },
-      y1: {
-        display: false,
-        type: "linear",
-        position: "left",
-        ticks: {
-          max: 100,
-          min: 0,
-        },
-      },
-      y2: {
-        display: true,
-        title: {
-          display: true,
-          text: unit,
-          color: "#212529",
-        },
-        ticks: {
-          color: "#212529",
-        },
-      },
-    },
-    plugins: {
-      datalabels: {
-        labels: {
-          display: false,
-        },
-      },
-      title: {
-        padding: {
-          top: 0,
-          bottom: 25,
-        },
-        font: {
-          size: 18,
-          weight: "bold",
-        },
-        display: true,
-        align: "center",
-        color: "#191558",
-        position: "top",
-        text: "Valeur ajoutée ",
-      },
-    },
-  };
   return (
     <div className="hidden">
       {printGrossImpact && (
@@ -423,25 +151,25 @@ console.log(valueAddedAreaFootprint)
       )}
 
       <Row className="graphs">
-        <Col>
+      <Col sm={4} xl={4} lg={4} md={4}>
           <Bar
             id={"print-Production-" + indic}
-            data={dataProduction}
-            options={optionsP}
+            data={productionDataConfig}
+            options={productionOptionConfig}
           />
         </Col>
-        <Col>
+        <Col sm={4} xl={4} lg={4} md={4}>
           <Bar
             id={"print-Consumption-" + indic}
-            data={dataConsumption}
-            options={optionsC}
+            data={consumptionDataConfig}
+            options={consumptionOptionConfig}
           />
         </Col>
-        <Col>
+        <Col sm={4} xl={4} lg={4} md={4}>
           <Bar
             id={"print-Value-" + indic}
-            data={dataValueAdded}
-            options={optionsV}
+            data={dataValueAddedConfig}
+            options={ValueAddedOptionConfig}
           />
         </Col>
       </Row>
@@ -449,9 +177,3 @@ console.log(valueAddedAreaFootprint)
   );
 };
 
-function roundNumber(num) {
-  if (num !== null) {
-    return parseFloat(num.toFixed(1));
-  }
-  return num;
-}
