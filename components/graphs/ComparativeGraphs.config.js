188f36ddcc21d1f2aa7d996fba1892739567f0fb
Laura Roost <laura.roost@lasocietenouvelle.org>
2022-10-14 16:42:49 +0200

    change graphs options

--- components/graphs/ComparativeGraphs.config.js
+++ components/graphs/ComparativeGraphs.config.js
@@ -1,52 +1,43 @@
-export const ComparativeGraphsData = (title, labels, dataset1, dataset2) => {
+export const ComparativeGraphsData = (title, dataset1, dataset2) => {
 
   let datasets = [
     {
       label: title,
       data: dataset1,
       backgroundColor: [
-        "RGBA(176,185,247,0.9)",
-        "RGBA(250,89,95,0.9)",
-        "RGBA(255,214,156,0.9)",
-      ],
-      borderWidth: 2,
-      borderColor: [
         "RGBA(176,185,247,1)",
         "RGBA(250,89,95,1)",
         "RGBA(255,214,156,1)",
       ],
+      borderWidth: 0,
       type: "bar",
-
+      barPercentage: 0.8,
+      categoryPercentage : 0.8
     },
   ];
   if (dataset2) {
     datasets.push({
-        spangaps: true,
-      label: "Objectif 2030",
+      label: "Objectifs 2030",
       data: [dataset2, null, dataset2],
-      backgroundColor: ["RGBA(176,185,247,0.5)", null, "RGBA(255,214,156,0.5)"],
-      borderWidth: 2,
-      borderColor: ["RGBA(176,185,247,0.6)", null, "RGBA(255,214,156,0.6)"],
+      backgroundColor: ["RGBA(215,220,251,1)", null, "RGBA(255,234,205,1)"],
+      borderWidth: 0,
+      barPercentage: 1,
+      categoryPercentage : 0.3
     });
   }
-
   const comparativeGraphsData = {
-    labels,
     datasets,
   };
   return comparativeGraphsData;
 };
 
-export const ComparativeGraphsOptions = (labels, title, unit) => {
+export const ComparativeGraphsOptions = (labels, unit) => {
   const options = {
+    devicePixelRatio: 2,
     scales: {
-      x: 
-        {
-          labels: labels,
-          barThickness: 30,
-          barPercentage: 1.0,
-          categoryPercentage : 1.0,
-        },
+      x : {
+        labels :  labels,
+      },
       y: 
         {
           display: true,
@@ -56,7 +47,7 @@ export const ComparativeGraphsOptions = (labels, title, unit) => {
             color: "#191558",
           },
           ticks: {
-            color: "#191558",
+            color: "#191558"
           },
         },
    },
@@ -70,19 +61,7 @@ export const ComparativeGraphsOptions = (labels, title, unit) => {
         },
       },
       title: {
-        padding: {
-          top: 0,
-          bottom: 25,
-        },
-        font: {
-          size: 18,
-          weight: "bold",
-        },
-        display: true,
-        align: "center",
-        position: "top",
-        color: "#191558",
-        text: title,
+        display: false,
       },
     },
   };
