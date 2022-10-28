e55032a3ab632c6f55b833015649d95c463ac928
Laura Roost <laura.roost@lasocietenouvelle.org>
2022-10-28 13:59:50 +0200

    align value to right

--- components/tables/ComparativeTable.js
+++ components/tables/ComparativeTable.js
@@ -52,78 +52,78 @@ export const ComparativeTable = ({
         <tr>
           <td>Agrégat</td>
           {targetSNBCarea.valueAddedTarget.value ? (
-            <td colSpan="2" className="border-left">France</td>
+            <td colSpan="2" className="border-left text-center">France</td>
           ) : (
-            <td className="border-left">France</td>
+            <td className="border-left text-center">France</td>
           )}
 
-          <td className="border-left">Exercice en cours</td>
+          <td className="border-left text-center">Exercice en cours</td>
           {printValue(productionDivisionFootprint.value, precision) &&
             printValue(consumptionDivisionFootprint.value, precision) &&
             printValue(valueAddedDivisionFootprint.value, precision) !==
-              " - " && <td colSpan="3" className="border-left">Branche</td>}
+              " - " && <td colSpan="3" className="border-left text-center">Branche</td>}
         </tr>
       </thead>
       <tbody>
         {indic == "ghg" && (
           <tr className="subth">
             <td scope="row"></td>
-            <td className="border-left">Valeur</td>
-            <td>Objectif 2030</td>
-            <td className="border-left">Valeur</td>
+            <td className="border-left text-end">Valeur</td>
+            <td className="text-end">Objectif 2030</td>
+            <td className="border-left text-end">Valeur</td>
             {productionDivisionFootprint.value && (
               <>
-                <td className="border-left">Valeur</td>
-                <td>Objectif 2030</td>
-                <td>Evolution</td>
+                <td className="border-left text-end">Valeur</td>
+                <td className="text-end">Objectif 2030</td>
+                <td className="text-end">Evolution</td>
               </>
             )}
           </tr>
         )}
         <tr>
           <td>Production</td>
-          <td className="border-left">
+          <td className="border-left text-end">
             {printValue(productionAreaFootprint.value, precision)}
             <span className="unit"> {unit}</span>
           </td>
           {indic == "ghg"  && (
-              <td>
+              <td className="text-end">
                 {Math.round(targetSNBCarea.productionTarget.value)} <span className="unit">{unit}</span>
               </td>
           )}
-          <td className="border-left">
+          <td className="border-left text-end">
             {printValue(production.footprint.getIndicator(indic).value, 1)}
             <span className="unit"> {unit}</span>
           </td>
           {printValue(productionDivisionFootprint.value, precision) !==
             " - " && (
-            <td className="border-left">
+            <td className="border-left text-end">
               {printValue(productionDivisionFootprint.value, precision)}
               <span className="unit"> {unit}</span>
             </td>
           )}
           {indic == "ghg" && productionDivisionFootprint.value && (
             <>
-              <td>
+              <td className="text-end">
                 {Math.round(targetSNBCbranch.productionTarget.value)}
-                <span className="unit">{unit}</span>
+                <span className="unit"> {unit}</span>
               </td>
-              <td>{productionEvolutionBranch} %</td>
+              <td className="text-end">{productionEvolutionBranch} %</td>
             </>
           )}
         </tr>
         <tr>
           <td>Consommations intermédiaires</td>
-          <td className="border-left">
+          <td className="border-left text-end">
             {printValue(consumptionAreaFootprint.value, precision)}
             <span className="unit"> {unit}</span>
           </td>
           {indic == "ghg"  && (
-              <td>
+              <td className="text-end">
                 {Math.round(targetSNBCarea.consumptionTarget.value)} <span className="unit">{unit}</span>
               </td>
           )}
-          <td className="border-left">
+          <td className="border-left text-end">
             {printValue(
               intermediateConsumption.footprint.getIndicator(indic).value,
               precision
@@ -132,51 +132,51 @@ export const ComparativeTable = ({
           </td>
           {printValue(consumptionDivisionFootprint.value, precision) !==
             " - " && (
-            <td className="border-left">
+            <td className="border-left text-end">
               {printValue(consumptionDivisionFootprint.value, precision)}
               <span className="unit"> {unit}</span>
             </td>
           )}
           {indic == "ghg" && productionDivisionFootprint.value && (
             <>
-              <td>
+              <td className="text-end">
                 {Math.round(targetSNBCbranch.consumptionTarget.value)}
-                <span className="unit">{unit}</span>
+                <span className="unit"> {unit}</span>
               </td>
-              <td>{consumptionEvolutionBranch} %</td>
+              <td className="text-end">{consumptionEvolutionBranch} %</td>
             </>
           )}
         </tr>
         <tr>
           <td>Valeur ajoutée</td>
-          <td className="border-left">
+          <td className="border-left text-end">
             {printValue(valueAddedAreaFootprint.value, precision)}
             <span className="unit"> {unit}</span>
           </td>
           {indic == "ghg"  && (
-              <td>
+              <td className="text-end">
                 {Math.round(targetSNBCarea.valueAddedTarget.value)}
-                <span className="unit">{unit}</span>
+                <span className="unit"> {unit}</span>
               </td>
           )}
-          <td className="border-left">
-            {printValue(netValueAdded.footprint.getIndicator(indic).value, 1)}
+          <td className="border-left text-end">
+            {printValue(netValueAdded.footprint.getIndicator(indic).value, precision)}
             <span className="unit"> {unit}</span>
           </td>
           {printValue(valueAddedDivisionFootprint.value, precision) !==
             " - " && (
-            <td className="border-left">
+            <td className="border-left text-end">
               {printValue(valueAddedDivisionFootprint.value, precision)}
               <span className="unit"> {unit}</span>
             </td>
           )}
           {indic == "ghg" && productionDivisionFootprint.value && (
             <>
-              <td>
+              <td className="text-end">
                 {Math.round(targetSNBCbranch.valueAddedTarget.value)}
                 <span className="unit"> {unit}</span>
               </td>
-              <td>{valueAddedEvolutionBranch}%</td>
+              <td className="text-end">{valueAddedEvolutionBranch}%</td>
             </>
           )}
         </tr>
