6c4105d8df99b33f5d80b6a168e27eba7f67b189
Laura Roost <laura.roost@lasocietenouvelle.org>
2022-11-03 10:17:16 +0100

    add td custom size for icon warning

--- components/sections/import/MappedAccounts.js
+++ components/sections/import/MappedAccounts.js
@@ -1,45 +1,54 @@
-
 import React, { useEffect, useState } from "react";
 import { Table } from "react-bootstrap";
 
-function MappedAccounts(props) 
-{
+function MappedAccounts(props) {
   const accounts = Object.entries(props.meta.accounts);
 
-  const depAccounts = accounts.filter((account) => /^28/.test(account) || /^29/.test(account) || /^39/.test(account));
-  const assetAccounts = accounts.filter((account) => /^2(0|1)/.test(account) || /^3[0-8]/.test(account));
+  const depAccounts = accounts.filter(
+    (account) =>
+      /^28/.test(account) || /^29/.test(account) || /^39/.test(account)
+  );
+  const assetAccounts = accounts.filter(
+    (account) => /^2(0|1)/.test(account) || /^3[0-8]/.test(account)
+  );
 
   const [isDisabled, setIsDisabled] = useState(true);
-  const [mappedAccounts, setMappedAccounts] = useState(props.meta.mappingAccounts);
+  const [mappedAccounts, setMappedAccounts] = useState(
+    props.meta.mappingAccounts
+  );
 
   // disabled if one account is not mapped i.e. enabled if all accounts are mapped
-  useEffect(() => 
-  {
-    if (Object.values(mappedAccounts).some(it => !it.accountAux)) {
+  useEffect(() => {
+    if (Object.values(mappedAccounts).some((it) => !it.accountAux)) {
       setIsDisabled(true);
     } else {
       setIsDisabled(false);
     }
   }, [isDisabled, mappedAccounts]);
 
-  function handleOnchange(depAccountNum, nextAccountAuxNum) 
-  {
+  function handleOnchange(depAccountNum, nextAccountAuxNum) {
     // remove association if a dep/amort. account is already associated with account aux
-    Object.entries(mappedAccounts).map(([key, {accountAux}]) => 
-    {
-      if ( key.substring(0,2) == depAccountNum.substring(0,2) && accountAux == nextAccountAuxNum) {
-        Object.assign(mappedAccounts, {[key]: {accountAux: "", directMatching: false},})
+    Object.entries(mappedAccounts).map(([key, { accountAux }]) => {
+      if (
+        key.substring(0, 2) == depAccountNum.substring(0, 2) &&
+        accountAux == nextAccountAuxNum
+      ) {
+        Object.assign(mappedAccounts, {
+          [key]: { accountAux: "", directMatching: false },
+        });
       }
     });
 
     // add association
-    Object.assign(mappedAccounts, {[depAccountNum]: {accountAux: nextAccountAuxNum, directMatching: true},});
+    Object.assign(mappedAccounts, {
+      [depAccountNum]: { accountAux: nextAccountAuxNum, directMatching: true },
+    });
 
     props.meta.mappingAccounts = mappedAccounts; // ?
-    setMappedAccounts({...mappedAccounts});
+    setMappedAccounts({ ...mappedAccounts });
 
-    // check if all accounts are associated 
-    if (Object.values(mappedAccounts).some(it => !it.accountAux)) {
+    // check if all accounts are associated
+    if (Object.values(mappedAccounts).some((it) => !it.accountAux)) {
       setIsDisabled(true);
     } else {
       setIsDisabled(false);
@@ -51,7 +60,7 @@ function MappedAccounts(props)
       <h3 className="subtitle ">
         Associez les comptes d'amortissements et de dépréciations
       </h3>
-      <Table size="lg"  hover className="mt-3">
+      <Table size="lg" hover className="mt-3">
         <thead>
           <tr>
             <th>Numéro de compte</th>
@@ -65,27 +74,40 @@ function MappedAccounts(props)
             <tr key={index}>
               <td>{depAccountNum}</td>
               <td>{depAccountLib}</td>
-              <td>{!mappedAccounts[depAccountNum].directMatching && <IconWarning />}</td>
+              <td style={{width:'40px'}}>
+                {!mappedAccounts[depAccountNum].directMatching && (
+                  <IconWarning />
+                )}
+              </td>
               <td>
-                <select className="form-control" onChange={(e) => handleOnchange(depAccountNum, e.target.value)} value={mappedAccounts[depAccountNum].accountAux || ""}>
+                <select
+                  className="form-select"
+                  onChange={(e) =>
+                    handleOnchange(depAccountNum, e.target.value)
+                  }
+                  value={mappedAccounts[depAccountNum].accountAux || ""}
+                >
                   <option value="">Sélectionner un compte...</option>
-                  {assetAccounts.filter(([assetAccountNum,_]) => assetAccountNum[0]==depAccountNum[0]).map(([assetAccountNum, assetAccountLib], index) => (
-                    <option key={index} value={assetAccountNum}>
-                      {assetAccountNum} - {assetAccountLib}
-                    </option>
-                  ))}
+                  {assetAccounts
+                    .filter(
+                      ([assetAccountNum, _]) =>
+                        assetAccountNum[0] == depAccountNum[0]
+                    )
+                    .map(([assetAccountNum, assetAccountLib], index) => (
+                      <option key={index} value={assetAccountNum}>
+                        {assetAccountNum} - {assetAccountLib}
+                      </option>
+                    ))}
                 </select>
               </td>
+
             </tr>
           ))}
         </tbody>
       </Table>
 
       <div className="text-end">
-      <button
-          className="btn btn-primary me-2"
-          onClick={() => props.return()}
-        >
+        <button className="btn btn-primary me-2" onClick={() => props.return()}>
           <i className="bi bi-chevron-left"></i> Retour aux A-Nouveaux
         </button>
         <button
@@ -105,8 +127,8 @@ export default MappedAccounts;
 
 const IconWarning = () => {
   return (
-    <span className="icon-warning" title="Informations à valider">
-      <i className=" bi bi-exclamation-triangle me-0"></i>
+    <span className="icon-warning" title="Informations à vérifier">
+      <i className=" bi bi-exclamation-triangle "></i>
     </span>
   );
 };
