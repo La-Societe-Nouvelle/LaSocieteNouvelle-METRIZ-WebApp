aabaf8f7dcd76271808aee421b50f11270e5e3a5
Laura Roost <laura.roost@lasocietenouvelle.org>
2022-11-15 10:34:45 +0100

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
+import { Analytics } from '@vercel/analytics/react';
+
 import '../styles/globals.scss'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp
