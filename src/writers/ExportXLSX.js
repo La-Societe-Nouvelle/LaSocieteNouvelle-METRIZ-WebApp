49787a52217ef21560d5b3026beab78c708d45d2
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2022-12-01 13:02:23 +0100

    init xlsx results writer

---
+++ src/writers/ExportXLSX.js
@@ -0,0 +1,71 @@
+import * as XLSX from 'xlsx';
+
+/* ---------- CONTENT WRITER ---------- */ 
+
+async function XLSXFileWriterFromJSON(fileProps,sheetName,jsonContent)
+// ...build XLSX File from JSON
+{
+  const worksheet = XLSX.utils.json_to_sheet(jsonContent);
+  
+  if (fileProps.wsclos != undefined) worksheet['!cols'] = fileProps.wsclos;
+
+  const workbook = XLSX.utils.book_new();
+        workbook.Props = fileProps;
+        workbook.SheetNames.push(sheetName);
+        workbook.Sheets[sheetName] = worksheet;
+
+  var XLSXData = XLSX.write(workbook, {bookType:'xlsx',  type: 'binary'});
+
+  // convert to ArrayBuffer
+  var buf = new ArrayBuffer(XLSXData.length);
+  var view = new Uint8Array(buf);
+  for (var i=0; i!=XLSXData.length; ++i) view[i] = XLSXData.charCodeAt(i) & 0xFF;
+
+  return buf;
+}
+
+/* ---------- CONTENT WRITER ---------- */ 
+
+async function XLSXFileIndicAggregatesWriter(workbook,content)
+// ...build XLSX File from JSON
+{
+  
+  const worksheet = XLSX.utils.table_to_sheet();
+  workbook.SheetNames.push(sheetName);
+        workbook.Sheets[sheetName] = worksheet;
+
+  var XLSXData = XLSX.write(workbook, {bookType:'xlsx',  type: 'binary'});
+
+  // convert to ArrayBuffer
+  var buf = new ArrayBuffer(XLSXData.length);
+  var view = new Uint8Array(buf);
+  for (var i=0; i!=XLSXData.length; ++i) view[i] = XLSXData.charCodeAt(i) & 0xFF;
+
+  return buf;
+}
+
+/* ---------- HEADER WRITER ---------- */ 
+
+async function XLSXHeaderFileWriter(fileProps,sheetName,arrayHeader)
+// ...build XLSX File (empty file with header) from array
+{
+  const worksheet = XLSX.utils.aoa_to_sheet(arrayHeader);
+  
+  if (fileProps.wsclos != undefined) worksheet['!cols'] = fileProps.wsclos;
+
+  const workbook = XLSX.utils.book_new();
+        workbook.Props = fileProps;
+        workbook.SheetNames.push(sheetName);
+        workbook.Sheets[sheetName] = worksheet;
+
+  workbook.Sheets[sheetName] = worksheet;
+
+  var XLSXData = XLSX.write(workbook, {bookType:'xlsx',  type: 'binary'});
+
+  // convert to ArrayBuffer
+  var buf = new ArrayBuffer(XLSXData.length);
+  var view = new Uint8Array(buf);
+  for (var i=0; i!=XLSXData.length; ++i) view[i] = XLSXData.charCodeAt(i) & 0xFF;
+
+  return buf;
+}
