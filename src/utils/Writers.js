8e382031ad8077f1787f77522ed2e0001be27b5a
Laura Roost <laura.roost@lasocietenouvelle.org>
2023-01-23 12:41:50 +0100

    base file for analysis and statement comment import

---
+++ src/utils/Writers.js
@@ -0,0 +1,98 @@
+import {
+  analysisTextWriterART,
+  analysisTextWriterECO,
+  analysisTextWriterGEQ,
+  analysisTextWriterGHG,
+  analysisTextWriterHAZ,
+  analysisTextWriterIDR,
+  analysisTextWriterKNW,
+  analysisTextWriterMAT,
+  analysisTextWriterNRG,
+  analysisTextWriterSOC,
+  analysisTextWriterWAS,
+  analysisTextWriterWAT,
+} from "../writers/analysis/analysis";
+
+import {
+  writeStatementART,
+  writeStatementECO,
+  writeStatementGEQ,
+  writeStatementGHG,
+  writeStatementHAZ,
+  writeStatementIDR,
+  writeStatementKNW,
+  writeStatementMAT,
+  writeStatementNRG,
+  writeStatementSOC,
+  writeStatementWAS,
+  writeStatementWAT,
+} from "../writers/deliverables/writeIndicstatement";
+
+export const getStatementNote = (impactsData, indic) => {
+  switch (indic) {
+    case "art":
+      return writeStatementART(impactsData);
+    case "idr":
+      return writeStatementIDR(impactsData);
+    case "eco":
+      return writeStatementECO(impactsData);
+    case "geq":
+      return writeStatementGEQ(impactsData);
+    case "ghg":
+      return writeStatementGHG(impactsData);
+    case "haz":
+      return writeStatementHAZ(impactsData);
+    case "knw":
+      return writeStatementKNW(impactsData);
+    case "mat":
+      return writeStatementMAT(impactsData);
+    case "nrg":
+      return writeStatementNRG(impactsData);
+    case "soc":
+      return writeStatementSOC(impactsData);
+    case "was":
+      return writeStatementWAS(impactsData);
+    case "wat":
+      return writeStatementWAT(impactsData);
+  }
+};
+
+export const getAnalyse = (
+  impactsData,
+  financialData,
+  comparativeData,
+  indic
+) => {
+  const session = {
+    impactsData: impactsData,
+    financialData: financialData,
+    comparativeData: comparativeData,
+  };
+
+  switch (indic) {
+    case "art":
+      return analysisTextWriterART(session);
+    case "idr":
+      return analysisTextWriterIDR(session);
+    case "eco":
+      return analysisTextWriterECO(session);
+    case "geq":
+      return analysisTextWriterGEQ(session);
+    case "ghg":
+      return analysisTextWriterGHG(session);
+    case "haz":
+      return analysisTextWriterHAZ(session);
+    case "knw":
+      return analysisTextWriterKNW(session);
+    case "mat":
+      return analysisTextWriterMAT(session);
+    case "nrg":
+      return analysisTextWriterNRG(session);
+    case "soc":
+      return analysisTextWriterSOC(session);
+    case "was":
+      return analysisTextWriterWAS(session);
+    case "wat":
+      return analysisTextWriterWAT(session);
+  }
+};
---
+++ src/writers/analysis/analysis.js
@@ -0,0 +1,27 @@
+import { analysisTextWriterART } from "./analysisTextWriterART";
+import { analysisTextWriterECO } from "./analysisTextWriterECO";
+import { analysisTextWriterGEQ } from "./analysisTextWriterGEQ";
+import { analysisTextWriterGHG } from "./analysisTextWriterGHG";
+import { analysisTextWriterHAZ } from "./analysisTextWriterHAZ";
+import { analysisTextWriterIDR } from "./analysisTextWriterIDR";
+import { analysisTextWriterKNW } from "./analysisTextWriterKNW";
+import { analysisTextWriterMAT } from "./analysisTextWriterMAT";
+import { analysisTextWriterNRG } from "./analysisTextWriterNRG";
+import { analysisTextWriterSOC } from "./analysisTextWriterSOC";
+import { analysisTextWriterWAS } from "./analysisTextWriterWAS";
+import { analysisTextWriterWAT } from "./analysisTextWriterWAT";
+
+export {
+    analysisTextWriterART,
+    analysisTextWriterECO,
+    analysisTextWriterGEQ,
+    analysisTextWriterGHG,
+    analysisTextWriterHAZ,
+    analysisTextWriterIDR,
+    analysisTextWriterKNW,
+    analysisTextWriterMAT,
+    analysisTextWriterNRG,
+    analysisTextWriterSOC,
+    analysisTextWriterWAS,
+    analysisTextWriterWAT
+}
