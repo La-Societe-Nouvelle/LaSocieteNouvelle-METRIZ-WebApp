a0a61b33d7d81b3c2d1716e7ad7d09a3ea7f4064
Laura Roost <laura.roost@lasocietenouvelle.org>
2023-06-12 11:47:34 +0200

    create results component

---
+++ components/sections/results/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Results = ({ session, publish }) => {
+  return (
+    <Container fluid>
+      <section className="step"></section>
+    </Container>
+  );
+};
+
+export default Results;
