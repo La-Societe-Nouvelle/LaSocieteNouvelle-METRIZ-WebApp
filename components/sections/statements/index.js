1cf636a4fe400dd7f4d6cd332ada88ca8775dd3a
Laura Roost <laura.roost@lasocietenouvelle.org>
2023-06-14 14:39:04 +0200

    select indics to declaire

--- components/sections/statements/index.js
+++ components/sections/statements/index.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
-import { Button, Col, Container, Image, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Image, Row } from "react-bootstrap";
 import indicators from "/lib/indics";
 import StatementComponent from "./parts/StatementComponent";
 
 const DirectImpacts = ({ session, submit }) => {
   const [period, setPeriod] = useState(session.financialPeriod);
+  const [selectedIndicators, setselectedIndicators] = useState([]);
+  const [showForms, setShowForms] = useState(false); // True if validations
+
   const [validations, SetValidations] = useState(
     session.validations[period.periodKey]
   );
@@ -13,23 +16,23 @@ const DirectImpacts = ({ session, submit }) => {
     Object.entries(indicators).filter(
       ([key, value]) =>
         value.isAvailable === true &&
-        indicators[key].category == "valueCreation"
+        indicators[key].category == "Création de la valeur"
     )
   );
 
-  const socialFootprintIndic = Object.fromEntries(
+  const socialFootprintIndics = Object.fromEntries(
     Object.entries(indicators).filter(
       ([key, value]) =>
         value.isAvailable === true &&
-        indicators[key].category == "socialFootprint"
+        indicators[key].category == "Empreinte sociale"
     )
   );
 
-  const EnvFootprintIndic = Object.fromEntries(
+  const EnvFootprintIndics = Object.fromEntries(
     Object.entries(indicators).filter(
       ([key, value]) =>
         value.isAvailable === true &&
-        indicators[key].category == "environmentalFootprint"
+        indicators[key].category == "Empreinte environnementale"
     )
   );
 
@@ -60,7 +63,7 @@ const DirectImpacts = ({ session, submit }) => {
   };
 
   const handleValidation = async (indic) => {
-    console.log(indic);
+
     SetValidations((validations) => [...validations, indic]);
 
     // add validation
@@ -71,16 +74,90 @@ const DirectImpacts = ({ session, submit }) => {
     await session.updateFootprints(period);
   };
 
+  const handleselectedIndicators = (event) => {
+    const isChecked = event.target.checked;
+    const indic = event.target.value;
+   
+    if (isChecked && !selectedIndicators.includes(indic)) {
+      console.log("vrai")
+      let indicators = selectedIndicators;
+      indicators.push(indic);
+      console.log(indicators)
+      setselectedIndicators(indicators);
+    }
+
+    if (isChecked == false && selectedIndicators.includes(indic)) {
+      console.log("faux")
+
+      let indicators = selectedIndicators;
+      const index = selectedIndicators.indexOf(indic);
+      indicators.splice(index, 1);
+      console.log(indicators)
+
+      setselectedIndicators(indicators);
+    }
+  };
   return (
     <Container fluid>
-      <section className="step">
+      {console.log(selectedIndicators)}
+      <section className="statement-section step">
         <h2 className="mb-3">Etape 4 - Déclaration des impacts directs </h2>
         <p>
           Identifiez et déclarez les impacts directs et obtenez des éléments
           d'analyse pour chaque indicateur clé.
         </p>
-        <div className="text-center p-3 mb-3 bg-light">
-          <h3 className="mb-0"> Création de la valeur</h3>
+        <div className="text-center p-3 mb-3 bg-light-secondary">
+          <h3 className="h4 mb-0 text-secondary text-uppercase">
+            {" "}
+            Création de la valeur
+          </h3>
+        </div>
+        <Row>
+          {Object.entries(valueCreationIndics).map(([key, value]) => (
+            <Col key={key} sm={4}>
+              <div className="border border-1 rounded p-5 mb-3 shadow-sm">
+                <div className="text-center">
+                  <Image
+                    className="me-2"
+                    src={"icons-ese/" + key + ".svg"}
+                    alt={key}
+                    height={80}
+                  />
+                  <h4 className="mt-4 w-75 m-auto">
+                    {value.libelle}
+                    {value.isBeta && <span className="beta ms-1">BETA</span>}
+                  </h4>
+                </div>
+                <Form className="indic-form text-center mt-4">
+                  <Form.Check
+                    type={"checkbox"}
+                    value={key}
+                    onChange={handleselectedIndicators}
+                  />
+                </Form>
+                {/* <div className="text-end">
+                    {validations.includes(key) && (
+                      <span className="display-6">
+                        <i className=" text-success ms-3 bi bi-patch-check"></i>
+                      </span>
+                    )}
+                  </div> */}
+              </div>
+              <div></div>
+            </Col>
+          ))}
+        </Row>
+        <div className="text-center p-3 mb-3 bg-light-secondary">
+          <h3 className="h4 mb-0 text-secondary text-uppercase">
+            {" "}
+            Empreinte sociale{" "}
+          </h3>
+        </div>
+        <div className="text-center p-3 mb-3 bg-light-secondary">
+          <h3 className="h4 mb-0 text-secondary text-uppercase">
+            {" "}
+            Empreinte environnementale{" "}
+          </h3>
         </div>
         <Row>
           {Object.entries(valueCreationIndics).map(([key, value]) => (
@@ -122,11 +199,8 @@ const DirectImpacts = ({ session, submit }) => {
           ))}
         </Row>
 
-        <div className="text-center p-3 mb-3 bg-light-secondary">
-          <h3 className="mb-0"> Empreinte sociale </h3>
-        </div>
         <Row>
-          {Object.entries(socialFootprintIndic).map(([key, value]) => (
+          {Object.entries(socialFootprintIndics).map(([key, value]) => (
             <Col key={key} sm={12}>
               <div className="border border-1 rounded p-3 mb-3 shadow-sm">
                 <div className="d-flex justify-content-between mb-3 border-bottom pb-3">
@@ -164,11 +238,9 @@ const DirectImpacts = ({ session, submit }) => {
             </Col>
           ))}
         </Row>
-        <div className="text-center p-3 mb-3 bg-light-secondary">
-          <h3 className="mb-0"> Empreinte environnementale </h3>
-        </div>
+
         <Row>
-          {Object.entries(EnvFootprintIndic).map(([key, value]) => (
+          {Object.entries(EnvFootprintIndics).map(([key, value]) => (
             <Col key={key} sm={12}>
               <div className="border border-1 rounded p-3 mb-3 shadow-sm">
                 <div className="d-flex justify-content-between mb-3 border-bottom pb-3">
