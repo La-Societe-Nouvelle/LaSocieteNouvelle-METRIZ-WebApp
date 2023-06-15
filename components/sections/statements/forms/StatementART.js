ef5a2788cd09e4576f7d9442721112535ebcb544
Laura Roost <laura.roost@lasocietenouvelle.org>
2023-06-15 14:34:12 +0200

    Display statement forms directly for validated indicators in the imported session

--- components/sections/statements/forms/StatementART.js
+++ components/sections/statements/forms/StatementART.js
@@ -139,6 +139,7 @@ const StatementART = (props) => {
             onChange={onIsValueAddedCraftedChange}
           />
         </Col>
+        
       </Form.Group>
       <Form.Group as={Row} className="form-group">
         <Form.Label column sm={4}>
