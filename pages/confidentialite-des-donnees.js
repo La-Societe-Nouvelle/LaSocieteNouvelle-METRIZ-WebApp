60e9ab33f8a4dffc4c4d7b6940bdf4995ecbdd56
Laura Roost <laura.roost@lasocietenouvelle.org>
2023-08-23 14:36:06 +0200

    fix HeaderPage import statement

--- pages/confidentialite-des-donnees.js
+++ pages/confidentialite-des-donnees.js
@@ -1,7 +1,7 @@
 // Components
 import { Container } from "react-bootstrap";
 import { Footer } from "../components/parts/Footer";
-import HeaderPage from "../components/parts/headers/HeaderPage";
+import {HeaderPage} from "../components/parts/headers/HeaderPage";
 
 export default function Page() {
   return (
