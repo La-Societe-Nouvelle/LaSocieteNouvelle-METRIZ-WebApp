7017f03761322b23ed9b550793bc64db5d0d775d
Laura Roost <laura.roost@lasocietenouvelle.org>
2023-12-05 11:14:24 +0100

    Refactor ProviderNumMode component - Implemented pagination for tables. - Modified the layout and styling of the form group and table.

--- src/components/sections/accountingImport/modals/ProviderNumMode.js
+++ src/components/sections/accountingImport/modals/ProviderNumMode.js
@@ -1,24 +1,16 @@
 // La Société Nouvelle
 
 // React
-import React, { useEffect, useState } from 'react';
-import { Col, Form, Row, Table } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import { Form, Row, Tab, Table, Tabs } from "react-bootstrap";
+import PaginationComponent from "../../providers/PaginationComponent";
 
 /* ####################################################################################################  */
 
-export const ProviderNumMode = ({
-  meta,
-  onSubmit,
-  onGoBack
-}) => {
-
-  const {
-    accountsProviders,
-    accountsAuxProviders
-  } = meta;
+export const ProviderNumMode = ({ meta, onSubmit, onGoBack }) => {
+  const { accountsProviders, accountsAuxProviders } = meta;
 
   const [useAccountAux, setUseAccountAux] = useState(true);
-  const [selectedTab, setSelectedTab] = useState("account");
 
   useEffect(() => {
     setUseAccountAux(true);
@@ -29,117 +21,57 @@ export const ProviderNumMode = ({
     setUseAccountAux(radioValue == "true");
   };
 
-  const switchTab = (event) => {
-    const table = event.target.value;
-    setSelectedTab(table);
-  };
-
   const submit = () => {
     meta.useAccountAux = useAccountAux;
     onSubmit();
   };
 
+  const handleInputChange = (event) => {
+  }
+
+  const generateAccountString = (accountsObj) => {
+    const numberOfAccounts = Object.entries(accountsObj).length;
+    const plural = numberOfAccounts > 1 ? "s" : "";
+    return `${numberOfAccounts} compte${plural}`;
+  }
+  
   return (
     <div>
       <h5>Gestion des comptes fournisseurs</h5>
-      <Form.Group as={Col} className="form-group align-items-center m-3">
-        <Row className="ms-1 text-start">
+      <Form.Group className="my-3">
+        <Row className="ms-1">
           <Form.Check
             inline
             type="radio"
-            label={"Gestion par compte fournisseur auxiliaire - "+Object.entries(accountsAuxProviders).length+" compte(s)"}
-            value="true"
-            checked={useAccountAux === true}
-            onChange={changeProviderNumRef}
-          />
-        </Row>
-        <Row className="ms-1 text-start">
-          <Form.Check
-            inline
-            type="radio"
-            label={"Gestion par compte fournisseur - "+Object.entries(accountsProviders).length+" compte(s)"}
+            label="Gestion par compte fournisseur"
+            id="Gestion par commpte fournisseur"
             value="false"
             checked={useAccountAux === false}
             onChange={changeProviderNumRef}
           />
         </Row>
+        <Row className="ms-1 mt-1">
+          <Form.Check
+            inline
+            type="radio"
+            label="Gestion par compte fournisseur auxiliaire"
+            id="Gestion par  compte fournisseur auxiliaire"
+            value="true"
+            checked={useAccountAux === true}
+            onChange={changeProviderNumRef}
+          />
+        </Row>
       </Form.Group>
-      <div>
-      <div className="table-menu mx-auto">
-        <button
-          key={1}
-          value="account"
-          onClick={switchTab}
-          className={selectedTab == "account" || "" ? "active" : ""}
-        >
-          Comptes fournisseurs
-        </button>
-        <button
-          key={2}
-          value="accountAux"
-          onClick={switchTab}
-          className={selectedTab == "accountAux" || "" ? "active" : ""}
-        >
-          Comptes fournisseurs auxiliaires
-        </button>
-      </div>
-      <div>
-        {(selectedTab=="account") &&
-          <Table size="sm" className="mt-4" striped>
-          <thead>
-            <tr>
-              <th className="px-1">Numéro de compte</th>
-              <th>Libellé</th>
-              <th className="text-end">Compte générique</th>
-            </tr>
-          </thead>
 
-          <tbody>
-            {Object.values(meta.accountsProviders).map(({ accountNum, accountLib }) => (
-              <tr key={accountNum}>
-                <td className="px-1">{accountNum}</td>
-                <td>{accountLib}</td>
-                <td className="px-4 text-end">
-                  <Form.Check
-                    type="checkbox"
-                    value={accountNum}
-                    //onChange={handleInputChange}
-                    checked={false}
-                  />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>}
-        {(selectedTab=="accountAux") &&
-          <Table size="sm" className="mt-4" striped>
-          <thead>
-            <tr>
-              <th className="px-1">Numéro de compte</th>
-              <th>Libellé</th>
-              <th className="text-end">Compte générique</th>
-            </tr>
-          </thead>
+      <Tabs defaultActiveKey="account" id="selectionTab">
+        <Tab eventKey="account" title={"Comptes fournisseurs (" + generateAccountString(accountsProviders) + ")"}>
+          <PaginatedTable data={Object.values(meta.accountsProviders)} onUpdate={handleInputChange} />
+        </Tab>
+        <Tab eventKey="accountAux" title={"Comptes fournisseurs (" + generateAccountString(accountsAuxProviders) + ")"}>
+          <PaginatedTable data={Object.values(meta.accountsAuxProviders)} onUpdate={handleInputChange} />
+        </Tab>
+      </Tabs>
 
-          <tbody>
-            {Object.values(meta.accountsAuxProviders).map(({ accountNum, accountLib }) => (
-              <tr key={accountNum}>
-                <td className="px-1">{accountNum}</td>
-                <td>{accountLib}</td>
-                <td className="px-4 text-end">
-                  <Form.Check
-                    type="checkbox"
-                    value={accountNum}
-                    //onChange={handleInputChange}
-                    checked={false}
-                  />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>}
-      </div>
-      </div>
       <div className="text-end">
         <button className="btn btn-primary me-2" onClick={() => onGoBack()}>
           <i className="bi bi-chevron-left"></i> Retour
@@ -149,6 +81,55 @@ export const ProviderNumMode = ({
           <i className="bi bi-chevron-right"></i>
         </button>
       </div>
+
+    </div>
+  );
+};
+
+const PaginatedTable = ({ data, onUpdate }) => {
+  const itemsPerPage = 10;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Pagination
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+
+  return (
+    <div>
+      <Table striped>
+        <thead>
+          <tr>
+            <th>Numéro de compte</th>
+            <th>Libellé</th>
+            <th className="text-end">Compte générique</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data
+            .slice(startIndex, endIndex)
+            .map(({ accountNum, accountLib }) => (
+              <tr key={accountNum + accountLib}>
+                <td>{accountNum}</td>
+                <td>{accountLib}</td>
+                <td className="px-4 text-end">
+                  <Form.Check
+                    type="checkbox"
+                    value={accountNum}
+                    onChange={onUpdate}
+                    checked={false}
+                  />
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+
+      <PaginationComponent
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={(newPage) => setCurrentPage(newPage)}
+      />
     </div>
   );
 };
