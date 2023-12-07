c70e5282510af181b0f54120d47c06e4e206ec59
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2023-12-07 15:26:00 +0100

    fixing import

--- config/api.js
+++ config/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { saveErrorLog } from '../src/services/StatsService';
+import { saveErrorLog } from '../src/statReportService/StatReportService';
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
