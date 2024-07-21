c19410dfd4b65642b1ff770c6f91c37c32a5ab5d
Sylvain HUMILIERE <sylvain.humiliere@la-societe-nouvelle.fr>
2024-07-21 23:08:13 +0200

    amélioration rapport ESE

--- src/components/sections/results/exports/reports/summaryReportGeneratorESE.js
+++ src/components/sections/results/exports/reports/summaryReportGeneratorESE.js
@@ -31,7 +31,8 @@ export const buildESEReport = async ({ session, period }) =>
   const validatedIndics = validations[period.periodKey];
 
   const corporateName = legalUnit.corporateName;
-  const { production } = financialData.mainAggregates;
+  const { providers, mainAggregates } = financialData;
+  const { production } = mainAggregates;
 
   const keyIndics = getKeyIndics(comparativeData.comparativeDivision);
   const indicatorLabels = getIndicatorLabels();
@@ -40,6 +41,8 @@ export const buildESEReport = async ({ session, period }) =>
 
   const valuableIndics = getValuableIndicators(production, period, indicatorLabels, keyIndics, comparativeData);
   const improvementIndics = getImprovementIndicators(production, period, indicatorLabels, keyIndics, comparativeData);
+
+  const transparentProviders = getTransparentProviders(providers,period);
 
   // ---------------------------------------------------------------
   // Charts
@@ -69,6 +72,7 @@ export const buildESEReport = async ({ session, period }) =>
   const content = 
   [
     // Page 1 - Graphiques récapitulatifs ----------------------------------------------------------------- //
+
     {
       text: "Empreinte Sociétale de l'Entreprise",
       style: "h1",
@@ -98,6 +102,23 @@ export const buildESEReport = async ({ session, period }) =>
       { text: "Points d'améliorations", style: "h2" },
       createImprovementTable(production, period, indicatorLabels, indicatorUnits, keyIndics, comparativeData)
     ] : [],
+
+    // Page 5 - Fournisseurs ------------------------------------------------------------------------------ //
+
+    { text: '', pageBreak: 'after' },
+    // table with main providers
+    { text: "Coopération des principaux fournisseurs (comptes auxiliaires)", style: "h2" },
+    createMainProvidersTable(providers, validatedIndics, period),
+    // table with provider who have published
+    ...transparentProviders.length>0 ? [
+      { text: "Fournisseurs ayant publié leur empreinte", style: "h2" },
+      createPublishedProviderFootprintsTable(providers, validatedIndics, period),
+    ] : [],
+
+    // Page 4 - Notice ------------------------------------------------------------------------------------ //
+
+    { text: '', pageBreak: 'after' },
+    buildNotePage(),
 
     // ---------------------------------------------------------------------------------------------------- //
   ];
@@ -140,7 +161,7 @@ export const buildESEReport = async ({ session, period }) =>
       },
       h3: {
         bold: true,
-        margin: [0, 0, 0, 10],
+        margin: [0, 10, 0, 5],
         fontSize: 8,
         color: colors.primary,
         font: "Raleway",
@@ -176,7 +197,7 @@ export const buildESEReport = async ({ session, period }) =>
         bold: false,
         color: colors.primary,
         font: "Raleway",
-        margin: [0, 0, 0, 20],
+        margin: [0, 0, 0, 5],
         alignment: "left",
         fontSize: 8
       },
@@ -209,7 +230,7 @@ const createSocialFootprintCharts = (indicatorLabels, indicatorImages) => {
 
       // indic footprint chart
       currentRow.push({
-        margin: [0, 10],
+        margin: [0, 5],
         stack: [
           { text: splitTitle(indicatorLabels[indic]), style: "h3", alignment: "center" },
           { image: image, width: 100, alignment: "center" }
@@ -569,7 +590,219 @@ const createImprovementTable = (production, period, indicatorLabels, indicatorUn
     style: 'table',
     margin: [0, 0, 0, 25],
   };
-};  
+}; 
+
+// ----------------------------------------------------------------------------------------------------
+// Main providers table
+
+const createMainProvidersTable = (providers, validatedIndics, period) => 
+{
+  const checkIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="#36c575" d="m9.55 18l-5.7-5.7l1.425-1.425L9.55 15.15l9.175-9.175L20.15 7.4z"></path></svg>';
+
+  // Main providers - purchasing
+  const mainPurchasingProviders = providers
+    .filter((provider) => !provider.isDefaultProviderAccount)
+    .filter((provider) => provider.periodsData.hasOwnProperty(period.periodKey) && provider.periodsData[period.periodKey].amountExpenses>0)
+    .sort((a,b) => b.periodsData[period.periodKey].amountExpenses - a.periodsData[period.periodKey].amountExpenses)
+    .slice(0,10)
+  
+  // Main providers - fixed asset
+  const mainFixedAssetProviders = providers
+    .filter((provider) => !provider.isDefaultProviderAccount)
+    .filter((provider) => provider.periodsData.hasOwnProperty(period.periodKey) && provider.periodsData[period.periodKey].amountInvestments>0)
+    .sort((a,b) => b.periodsData[period.periodKey].amountInvestments - a.periodsData[period.periodKey].amountInvestments)
+    .slice(0,5)
+
+  const tableBody = [
+    [
+      { text: 'Compte', style: 'tableHeader' },
+      { text: 'SIREN', style: 'tableHeader' },
+      { text: 'Dénomination sociale / Libellé du compte', style: 'tableHeader' },
+      { text: 'Publication', style: 'tableHeader', alignment: "center" },
+      { text: 'Indicateurs publiés', style: 'tableHeader', alignment: "center" }
+    ],
+  ];
+
+  // Purchasing
+  tableBody.push([
+    { text: "Fournisseurs d'achats", bold: true, italics: true, colSpan: "5" }
+  ])
+
+  mainPurchasingProviders
+    .forEach((provider) => 
+    {
+      const isProviderIdentified = !provider.useDefaultFootprint;
+      const nbPublishedIndics = Object.values(provider.footprint.indicators)
+        .filter((indicator) => indicator.flag == "p").length;
+        // .filter((indicator) => validatedIndics.includes(indicator.indic) && indicator.flag == "p").length;
+
+      tableBody.push([
+        { text: provider.providerNum },
+        { text: provider.legalUnitData.siren || "-" },
+        { text: provider.legalUnitData.denomination || provider.providerLib },
+        nbPublishedIndics > 0 ? { svg: checkIcon, width: 7, height: 7, alignment: "center" } : { text: "-", style: 'data', alignment: "center" },
+        { text: isProviderIdentified ? nbPublishedIndics+"/"+"12" : "-", alignment: "center" }
+      ]);
+    });
+
+  // Fixes asset
+  if (mainFixedAssetProviders.length>0)
+  {
+    tableBody.push([
+      { text: "Fournisseurs d'immobilisations", bold: true, italics: true, colSpan: "5" }
+    ])
+  
+    mainFixedAssetProviders
+      .forEach((provider) => 
+      {
+        const isProviderIdentified = !provider.useDefaultFootprint;
+        const nbPublishedIndics = Object.values(provider.footprint.indicators)
+          .filter((indicator) => indicator.flag == "p").length;
+          // .filter((indicator) => validatedIndics.includes(indicator.indic) && indicator.flag == "p").length;
+  
+        tableBody.push([
+          { text: provider.providerNum },
+          { text: provider.legalUnitData.siren || "-" },
+          { text: provider.legalUnitData.denomination || provider.providerLib },
+          nbPublishedIndics > 0 ? { svg: checkIcon, width: 7, height: 7, alignment: "center" } : { text: "-", style: 'data', alignment: "center" },
+          { text: isProviderIdentified ? nbPublishedIndics+"/"+"12" : "-", alignment: "center" }
+        ]);
+      });
+  }
+
+  return {
+    table: {
+      headerRows: 1,
+      widths: ['auto', 50, '*', 'auto', 'auto'],
+      body: tableBody,
+    },
+    layout: tableLayout(),
+    style: 'table',
+    margin: [0, 0, 0, 25],
+  };
+}; 
+
+
+// ----------------------------------------------------------------------------------------------------
+// Published provider footprints table
+
+const createPublishedProviderFootprintsTable = (providers, validatedIndics, period) => 
+{
+  const checkIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="#36c575" d="m9.55 18l-5.7-5.7l1.425-1.425L9.55 15.15l9.175-9.175L20.15 7.4z"></path></svg>';
+
+  // Main providers - purchasing
+  const transparentProviders = providers
+    .filter((provider) => !provider.isDefaultProviderAccount)
+    .filter((provider) => !provider.useDefaultFootprint)
+    .filter((provider) => provider.periodsData.hasOwnProperty(period.periodKey))
+    .filter((provider) => Object.values(provider.footprint.indicators).some((indicator) => indicator.flag == "p"))
+    .sort((a,b) => a.legalUnitData.denomination.localeCompare(b.legalUnitData.denomination))
+  
+  const tableBody = [
+    [
+      { text: 'Compte', style: 'tableHeader' },
+      { text: 'SIREN', style: 'tableHeader' },
+      { text: 'Dénomination sociale', style: 'tableHeader' },
+      { text: 'Indicateurs publiés', style: 'tableHeader', alignment: "center" }
+    ],
+  ];
+
+  transparentProviders
+    .forEach((provider) => 
+    {
+      const nbPublishedIndics = Object.values(provider.footprint.indicators)
+        .filter((indicator) => validatedIndics.includes(indicator.indic) && indicator.flag == "p").length;
+
+      tableBody.push([
+        { text: provider.providerNum },
+        { text: provider.legalUnitData.siren || "error" },
+        { text: provider.legalUnitData.denomination || "error" },
+        { text: nbPublishedIndics+"/"+validatedIndics.length, alignment: "center" }
+      ]);
+    });
+
+  return {
+    table: {
+      headerRows: 1,
+      widths: ['auto', 50, '*', 'auto'],
+      body: tableBody,
+    },
+    layout: tableLayout(),
+    style: 'table',
+    margin: [0, 0, 0, 25],
+  };
+}; 
+  
+// ----------------------------------------------------------------------------------------------------
+// Notes
+
+const buildNotePage = () => 
+{
+  const indicsNotes = {
+    eco: "L'indicateur exprime la part (en %) du chiffre d'affaires produit en France i.e. dont la valeur ajoutée est localisée en France. Le complément indique la part des importations directes ou indirectes liées à la production de l'entreprise.",
+    art: "L'indicateur exprime la part (en %) du chiffre d'affaires faisant intervenir la valeur ajoutée d'entreprises artisanales ou dont le savoir faire est reconnu (ex. label EPV). Comem pour l'intérêt social, l'indicateur inclut la valeur ajoutée nette de l'entreprise si celle-ci répond à ces critères. L'indicateur vise à valoriser les savoir-faire et les métiers de l'artisanat.",
+    soc: "L'indicateur exprime la part (en %) du chiffre d'affaires faisant intervenir la valeur ajoutée de structures de l'Economie Sociale et Solidaire (ESS), de sociétés à mission ou d'entreprises ayant défini une raison d'être (dans le sens de la loi Pacte). Il inclut la valeur ajoutée nette de l'entreprise si celle-ci cherche à répondre à un enjeu sociétal. L'indicateur vise ainsi à soutenir l'ESS et à encourager chaque entreprise à définir sa raison d'être.",
+    idr: "L'indice correspond au rapport interdécile D9/D1 des taux horaires au sein des effectifs (rapport entre le taux horaire le plus bas des 10% les plus élevés et le plus haut des 10% les plus faibles) ; sa valeur est de 1 pour les entreprises individuelles. L'indicateur fait intervenir l'indice interne de l'entreprise mais également celui associé aux fournisseurs, au prorata de leur poids dans les consommations et investissements.",
+    geq: "L'indicateur correspond à l'écart brut entre le taux horaire brut moyen des femmes et des hommes (exprimé en pourcentage du taux horaire moyen). De la même manière que pour les autres indicateurs, il fait les écarts asociés aux fournisseurs, au prorata de leur poids dans les consommations et investissements.",
+    knw: "L'indicateur exprime la part (en %) du chiffre d'affaires fléchée vers la recherche, la formation ou l'enseignement. A l'échelle de l'entreprise, il prend en compte les rémunérations des contrats d'apprentissages et de professionnalisation (et gratification de stages), les rémunérations liées à du temps de formation (participant ou animateur) ou des travaux de recherche, aux divers taxes et participations à la formation professionnelle.",
+    ghg: "L'indicateur exprime la quantité émise de gaz à effet de serre par euro de chiffre d'affaires (en gCO2e/€).",
+    nrg: "L'indicateur exprime la quantité consommée d'énergie par euro de chiffre d'affaires (en kJ/€).",
+    mat: "L'indicateur exprime la quantité extraite de matières premières (biomasse, matières fossiles et minerais) par euro de chiffre d'affaires (en g/€).",
+    wat: "L'indicateur exprime la quantité consommée d'eau par euro de chiffre d'affaires (en L/€). Il ne prend pas les prélèvements avec restitution dans le milieu d'origine sans traitement (sans modification de l'état).",
+    was: "L'indicateur exprime la quantité produite de déchets par euro de chiffre d'affaires (en g/€).",
+    haz: "L'indicateur exprime la quantité utilisée de produits chimiques dangereux par euro de chiffre d'affaires (en g/€). L'indicateur s'appuie sur la réglementation REACH pour les déclarations."
+  }
+
+  const methodologyNotes = [
+    "L'empreinte sociétale est un panel d'indicateurs relatif aux externalités sociales et environnementales de la production de l'entreprise. Pour chaque indicateur, la mesure prend donc en compte les impacts liées à la valeur ajoutée (impacts directs des opérations) et les impacts indirects liés aux consommations, intermédiaires et de capital fixe. La méthodologie est publique et libre d'exploitation.",
+    "Pour les impacts directs, la mesure s'appuie sur les caractéristiques de l'entreprise et des données d'activités : consommations (eau, énergie, carburants, etc.), données sociales, etc.",
+    "Pour les impacts indirects liés aux consommations, ceux-ci sont estimés à partir de l'empreinte sociétale de vos fournisseurs. Elles sont disponibles au sein de notre base de données ouverte. En l'absence de données pubiées, des données statistiques sont utilisées selon le profil de l'entreprise (activité, taille, etc.). De la même manière, votre empreinte sera utilisée par vos clients pour la mesure de l'empreinte de leurs activités.",
+    "La description complète de la méthodologie est accessible ici : https://docs.lasocietenouvelle.org/empreinte-societale/mesure"
+  ]
+
+  const uncertaintyNotes = [
+    "Les incertitudes sont exprimées en pourcentage de la valeur concernée. Elles proviennent de l'utilisation de valeurs statistiques pour l'estimation des impacts indirects, dès lors qu'un fournisseur ne fait pas preuve de transparence sur l'empreinte de ses activités. Dans une moindre mesure et uniquement pour les indicateurs environnementaux, une incertitude intervient lors de la mesure des impacts directs (la mesure d'une grandeur physique ne pouvant être exacte).",
+    "Selon les indicateurs et les secteurs, la statistique publique et les remontées de données microcéconomiques ne permettent pas toujours de confirmer l'empreinte par défaut affectée à une entreprise selon ses caractéristiques publiques (activité, taille, catégorie juridique, etc.) : certaines valeurs sont donc fournies avec une incertitude parfois élevée (supérieure à 75 %).",
+    "La réduction de l'incertitude liée aux résultats obtenus passe par la publication de l'empreinte sociétale de chaque entreprise, et par l'envoi de rapports statistiques anonymes permettant d'accélérer les travaux de modélisation."
+  ]
+
+  const targetNotes = [
+    " - Ecart de rémunérations femmes/hommes : Objectif d'écart de 0% en 2050 (source: La Société Nouvelle)",
+    " - Emissions de gaz à effet de serre : Budgets carbone sectoriels de la Stratégie Nationale Bas-Carbone (SNBC), étendus jusqu'à 2030",
+    " - Contribution à l'évolution des compétences et des connaissances : Augmentation de 0.8 pt (pourcentage du PIB) d'ici 2030 par rapport à 2020, pour atteindre les 3% du PIB",
+    " - Extraction de matières premières : Quantité extraite de matières premières stable (source: La Société Nouvelle)",
+    " - Consommation d'énergie : Planification Pluriannuelle de l'Energie (PPE)",
+    " - Contribution aux acteurs d'intérêt social : Objectif de 100% en 2050 (source: La Société Nouvelle)",
+    " - Production de déchets : Plan national de prévention des déchets (PNPD)",
+    " - Consommation d'eau : Consommation d'eau stable (source: La Société Nouvelle)"
+  ]
+
+  const indics = ["eco","soc","art","idr","geq","knw","ghg","nrg","wat","mat","was","haz"]; // ordered
+
+  const content = 
+  [
+    { text: "Notice", style: "h1" },
+    { text: "Description des indicateurs", style: "h2" },
+    ...indics.map((indic) => 
+    [
+      { text: metaIndics[indic].libelle, style: "h3" },
+      { text: indicsNotes[indic], style: "paragraph", alignment: "justify"}
+    ]),
+    { text: '', pageBreak: 'after' },
+    { text: "Méthodologie de mesure", style: "h2" },
+    methodologyNotes.map((note) => {return({ text: note, style: "paragraph", alignment: "justify"})}),
+    { text: "Données utilisées", style: "h2" },
+    { text: "Les données utilisées sont des données ouvertes fournies par La Société Nouvelle (https://lasocietenouvelle.org). Elles proviennent de publications volontaires de la part d'entreprises et de travaux statistiques réalisés par La Société Nouvelle.", style: "paragraph"},
+    { text: "Incertitudes", style: "h2" },
+    uncertaintyNotes.map((note) => {return({ text: note, style: "paragraph", alignment: "justify"})}),
+    { text: "Moyennes et objectifs sectoriels", style: "h2" },
+    { text: "Les moyennes sectorielles correspondent à l'empreinte de la production de la branche de référence de l'entreprise (obtenue à partir de son code APE). Les valeurs sont obtenues à partir des travaux de modélisation statistiques de La Société Nouvelle.", style: "paragraph"},
+    { text: "La méthodologie et les sources de données utilisées pour chaque indicateur sont accessibles ici : https://docs.lasocietenouvelle.org/series-donnees/macro_fpt_a88.", style: "paragraph"},
+    { text: "Les objectifs sectiels 2030 s'appuie sur les travaux de prospectives économiques (modélisation de l'économie française sur les années à venir) et les cibles suivantes : \n"+targetNotes.join("\n"), style: "paragraph"}
+  ];
+
+  return content;
+};
 
 // ########################################################################################################### //
 // ################################################## UTILS ################################################## //
@@ -737,11 +970,16 @@ const getImprovementIndicators = (production, period, indicatorLabels, keyIndics
   });
 };
 
-const getNationalTargets = (indicatorLabels, comparativeData) => {
-  return Object.keys(indicatorLabels).filter(key => {
-    return getTargetValue(comparativeData, key) !== null;
-  });
-};
+const getTransparentProviders = (providers,period) => {
+  const transparentProviders = providers
+    .filter((provider) => !provider.isDefaultProviderAccount)
+    .filter((provider) => !provider.useDefaultFootprint)
+    .filter((provider) => provider.periodsData.hasOwnProperty(period.periodKey))
+    .filter((provider) => Object.values(provider.footprint.indicators).some((indicator) => indicator.flag == "p"))
+    .sort((a,b) => a.legalUnitData.denomination.localeCompare(b.legalUnitData.denomination));
+  return transparentProviders
+}
+
 // ----------------------------------------------------------------------------------
 
 const getEffortPercentage = (currentValue, targetValue) => {
@@ -768,7 +1006,7 @@ const createTableTitle = (label) => {
       style: {
         fontSize: 11,
         bold: true,
-        margin: [0, 10, 0, 10],
+        margin: [0, 10, 0, 0],
         color: "#fa595f",
         font: "Raleway",
         border: "2px"
@@ -795,7 +1033,7 @@ const createTableTitle = (label) => {
       paddingTop: function (i, node) { return 2 },
       paddingBottom: function (i, node) { return 2 },
     },
-    margin: [0, 10, 0, 10],
+    margin: [0, 10, 0, 5],
   };
 };
 
